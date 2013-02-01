62d54c8ce3fdfc539db70c4af55f89156d5b99c1
Marcel Luethi <marcel.luethi@unibas.ch>
2013-02-01 13:33:13 +0100

    Changed registration test to reflect new structure

--- src/main/scala/smptk/registration/Test.scala
+++ src/main/scala/smptk/registration/Test.scala
@@ -3,81 +3,65 @@ package smptk.registration
 import scala.collection.immutable.{ Vector => SVector }
 import TransformationSpace.ParameterVector
 import Registration.RegistrationResult
-
 import breeze.plot._
 import breeze.linalg._
+import smptk.image.ContinuousScalarImage1D
+import smptk.image.ContinuousDomain
+import smptk.image.Geometry._
+import smptk.image.ContinuousImageDomain1D
+import smptk.image.ContinuousScalarImageLike
 
 object Test {
 
-//  type Space = Space1D
-//
-//  case class Simple1DDomain(val origin: Float, val extent: DenseVector[Float]) extends ImageDomain[Space1D]() {
-//    def dimensionality = 1
-//    def isInside(pt: Space1D#Point) = pt > origin && pt < origin + extent(0)
-//    def uniformSamples(n: Int) = {
-//      val spacing: Float = extent(0) / n
-//      (0 until n) map (i => (origin + i * spacing))
-//    }
-//  }
-//
-//  // a concrete example of a registration
-//  def doRegistration(): RegistrationResult[Space] = {
-//
-//    val oneDTransformSpace = new TransformationSpace[Space] {
-//      def apply(p: ParameterVector) = {
-//        new Transformation[Space] { def apply(pt: Space#Point) = p(0) * pt }
-//      }
-//      def parametersDimensionality: Int = 1
-//      def takeDerivative(p: ParameterVector) = { x: Space#Point =>
-//        val M = DenseMatrix.zeros[Float](1, 1)
-//        M(0, 0) = x
-//        M
-//      }
-//
-//    }
-//
-//    val metric = new ImageMetric[Space] {
-//      def apply(img1: ContinuousScalarImage[Space],
-//        img2: ContinuousScalarImage[Space]) = {
-//        (img1 - img2).squaredNorm
-//
-//      }
-//      def takeDerivativeWRTToMovingImage(img1: ContinuousScalarImage[Space],
-//        img2: ContinuousScalarImage[Space]) = {
-//        (img1 - img2) * 2f
-//      }
-//    }
-//
-//    val fixedImage = new ContinuousScalarImage[Space] {
-//      val domain = Simple1DDomain(-5, DenseVector(10f))
-//      def apply(x: Space#Point) = x * x
-//      def takeDerivative(x: Space#Point) = DenseVector(2 * x)
-//
-//    }
-//
-//    val movingImage = new ContinuousScalarImage[Space] {
-//      val domain = Simple1DDomain(-5, DenseVector(10f))
-//      def apply(x: Space#Point) = 3 * x * x
-//      def takeDerivative(x: Space#Point) = DenseVector(6 * x)
-//
-//    }
-//
-//    val initialParameters = DenseVector(1f) // the 0 vector
-//    val regularizer = new Regularizer {
-//      def apply(p: ParameterVector) = 0f
-//    }
-//
-//    val regResult = Registration.registration(fixedImage, movingImage, oneDTransformSpace, metric, regularizer, initialParameters)
-//
-//    val f = Figure()
-//    val p = f.subplot(0)
-//    val x = fixedImage.domain.uniformSamples(100)
-//    
-//    //p += plot(x, x.map(fixedImage))
-//    //p += plot(x, x.map(movingImage compose regResult.transform), '+')
+
+
+// a concrete example of a registration
+	def doRegistration(): RegistrationResult[CoordVector1D] = {
+
+    val oneDTransformSpace = new TransformationSpace[CoordVector1D] {
+      def apply(p: ParameterVector) = {
+        new Transformation[CoordVector1D] { def apply(pt: Point1D) = p(0) * pt(0) }
+      }
+      def parametersDimensionality: Int = 1
+      def takeDerivative(p: ParameterVector) = { x: Point1D =>
+        val M = DenseMatrix.zeros[Float](1, 1)
+        M(0, 0) = x(0)
+        M
+      }
+    }
+
+    val metric = new ImageMetric[CoordVector1D] {
+    	def apply(img1: ContinuousScalarImageLike[CoordVector1D],
+        img2: ContinuousScalarImageLike[CoordVector1D]) = {
+    		(img1 - img2).squaredNorm
+      }
+      def takeDerivativeWRTToMovingImage(img1: ContinuousScalarImageLike[CoordVector1D],
+        img2: ContinuousScalarImageLike[CoordVector1D]) = {
+        (img1 - img2) * 2f
+      }
+    }
+
+    val domain = ContinuousImageDomain1D(-5f, 10f)    
+    val fixedImage  = ContinuousScalarImage1D(domain, x => x(0) * x(0), x => DenseVector(2 * x(0)))
+    val movingImage = ContinuousScalarImage1D(domain, x => 3 * x(0) * x(0), x => DenseVector(6 * x(0)))
+
+    val initialParameters = DenseVector(1f)
+    val regularizer = new Regularizer {
+      def apply(p: ParameterVector) = 0f
+    }
+
+    val regResult = Registration.registration(fixedImage, movingImage, oneDTransformSpace, metric, regularizer, initialParameters)
+
+    val f = Figure()
+    val p = f.subplot(0)
+    val x = fixedImage.domain.uniformSamples(100).map(_.toFloat)
+    
+    p += plot(x, x.map(f => fixedImage(f)))
+    val warpedimg = movingImage compose regResult.transform
+    p += plot(x, x.map(f => warpedimg(f)), '+')
 //    val alpha : DenseVector[Float]= linspace(-1, 1).map(_.toFloat)
-//    def warpedImage(alpha : DenseVector[Float]) = new ContinuousScalarImage[Space] { 
-//      def apply(x : Space#Point) = movingImage(oneDTransformSpace(alpha)(x))
+//    def warpedImage(alpha : DenseVector[Float]) =[Space] { 
+//      def apply(x : Point1D) = movingImage(oneDTransformSpace(alpha)(x))
 //      def takeDerivative(x : Space#Point) = DenseVector(0)
 //      def domain = fixedImage.domain
 //    }
@@ -90,13 +74,14 @@ object Test {
 //      def apply(x: Space#Point) = DenseVector(x * x, x* x)
 //      def takeDerivative(x: Space#Point) = DenseVector(0)
 //    }
-//    regResult
-//  }
+    regResult
+	}
+
 
   def main(args: Array[String]) {
-    //val regResult = doRegistration()
-    //println("optimal parameters: " + regResult.parameters.toString)
-    println("hello world")
+    val regResult = doRegistration()
+    println("optimal parameters: " + regResult.parameters.toString)
+    
   }
 
 }
