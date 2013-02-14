5f66138947b68a6cebcaafca2ead2d1ebf93942b
Marcel Luethi <marcel.luethi@unibas.ch>
2013-02-14 21:41:18 +0100

    added method for sampling 1d images

--- src/main/scala/smptk/image/Resample.scala
+++ src/main/scala/smptk/image/Resample.scala
@@ -11,4 +11,14 @@ object Resample {
     })
     DiscreteScalarImage2D(domain, sampledValues)
   }
+
+ def sample[@specialized(Short, Float) Pixel: ScalarPixel : ClassTag](img: ContinuousScalarImage1D, domain: DiscreteImageDomain1D, outsideValue: Pixel): DiscreteScalarImage1D[Pixel] = {
+    val scalarPixel = implicitly[ScalarPixel[Pixel]]
+    val sampledValues = domain.points.map(pt => {
+      if (img.isDefinedAt(pt)) scalarPixel.fromFloat(img(pt))
+      else outsideValue
+    })
+    DiscreteScalarImage1D(domain, sampledValues)
+  }
 }
+
