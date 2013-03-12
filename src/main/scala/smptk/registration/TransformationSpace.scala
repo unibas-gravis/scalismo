ec7b29a0bcf5da30cce5c0d47f25e986a5a5a1b8
Marcel Luethi <marcel.luethi@unibas.ch>
2013-03-12 11:55:05 +0100

    1) Added RotationTransformationSpace2D 2) Added ProductTransformationSpace together with tests for combinations of rotation and translation.

--- src/main/scala/smptk/registration/TransformationSpace.scala
+++ src/main/scala/smptk/registration/TransformationSpace.scala
@@ -4,28 +4,77 @@ package registration
 import scala.language.higherKinds
 import TransformationSpace.ParameterVector
 import breeze.linalg.DenseVector
+import breeze.linalg._
 import breeze.linalg.DenseMatrix
 import image._
 import smptk.image.Geometry._
 
 trait TransformationSpace[CV[A] <: CoordVector[A]] extends Function1[ParameterVector, Transformation[CV]] {
+  self: TransformationSpace[CV] =>
+
   type JacobianImage = Function1[CV[Float], DenseMatrix[Float]]
   def parametersDimensionality: Int
   def takeDerivativeWRTParameters(alpha: ParameterVector): JacobianImage
+
+  // 
+  def product(that: TransformationSpace[CV]): TransformationSpace[CV] = {
+    ProductTransformationSpace(self, that)
+  }
+
 }
 
 trait Transformation[CV[A] <: CoordVector[A]] extends (CV[Float] => CV[Float]) {
-  def takeDerivative(x : CV[Float]): DenseMatrix[Float]
+  def takeDerivative(x: CV[Float]): DenseMatrix[Float]
+}
+
+
+case class ProductTransformationSpace[CV[A] <: CoordVector[A]](outer : TransformationSpace[CV], inner  : TransformationSpace[CV]) extends TransformationSpace[CV] {
+
+  def parametersDimensionality = outer.parametersDimensionality + inner.parametersDimensionality
+
+  def apply(p: ParameterVector) = {
+
+    new Transformation[CV] {
+      def apply(x: CV[Float]) = {
+        val (pThis, pThat) = splitProductParameterVector(p)
+        val outerTransform = outer(pThis)
+        val innerTransfrom = inner(pThat)
+        (outerTransform compose innerTransfrom)(x)
+      }
+      def takeDerivative(x: CV[Float]) = {
+        val (pThis, pThat) = splitProductParameterVector(p)
+        val outerTransform = outer(pThis)
+        val innerTransform = inner(pThat)
+        outerTransform.takeDerivative(innerTransform(x)) * innerTransform.takeDerivative(x)
+      }
+    }
+  }
+
+  def takeDerivativeWRTParameters(p: ParameterVector) = {
+
+    val split = splitProductParameterVector(p)
+    (x: CV[Float]) => DenseMatrix.horzcat(
+        outer.takeDerivativeWRTParameters(split._1)(x), 
+        inner.takeDerivativeWRTParameters(split._2)(x))
+  }
+
+  private def splitProductParameterVector(p: ParameterVector): (ParameterVector, ParameterVector) = {
+      val pThis = p.slice(0, outer.parametersDimensionality, 1) // +1 apparently excludes end
+      val pThat = p.slice(outer.parametersDimensionality, p.length, 1)
+      (pThis, pThat)
+    }
+
+  
 }
 
 case class TranslationSpace1D extends TransformationSpace[CoordVector1D] {
   def apply(p: ParameterVector) = {
-    new Transformation[CoordVector1D] { 
-      
-      def apply(pt: Point1D) =   p(0) + pt(0)
-      def takeDerivative(x : Point1D) = {
+    new Transformation[CoordVector1D] {
+
+      def apply(pt: Point1D) = p(0) + pt(0)
+      def takeDerivative(x: Point1D) = {
         DenseMatrix.eye[Float](1)
-      } 
+      }
     }
   }
   def parametersDimensionality: Int = 1
@@ -36,22 +85,57 @@ case class TranslationSpace1D extends TransformationSpace[CoordVector1D] {
 
 case class TranslationSpace2D extends TransformationSpace[CoordVector2D] {
 
-   def parametersDimensionality: Int = 2
-    
+  def parametersDimensionality: Int = 2
+
   def apply(p: ParameterVector) = {
-    new Transformation[CoordVector2D] { 
-      def apply(pt: Point2D) = CoordVector2D(p(0) + pt(0), p(1) + pt(1)) 
-      def takeDerivative(x : Point2D) = {
+    new Transformation[CoordVector2D] {
+      def apply(pt: Point2D) = CoordVector2D(p(0) + pt(0), p(1) + pt(1))
+      def takeDerivative(x: Point2D) = {
         DenseMatrix.eye[Float](2)
-      } 
+      }
     }
-   }
+  }
 
   def takeDerivativeWRTParameters(p: ParameterVector) = { x: Point2D =>
     DenseMatrix.eye[Float](2)
   }
 }
 
+case class RotationSpace2D(val centre: CoordVector2D[Float]) extends TransformationSpace[CoordVector2D] {
+
+  def parametersDimensionality: Int = 1 //  angle
+  def rotationParametersToParameterVector(phi: Float): ParameterVector = {
+    DenseVector(phi)
+  }
+  def apply(p: ParameterVector) = {
+    require(p.length == 1)
+
+    val rotMatrix = DenseMatrix((math.cos(p(0)), -math.sin(p(0))), (math.sin(p(0)), math.cos(p(0))))
+
+    new Transformation[CoordVector2D] {
+      def apply(pt: Point2D) = {
+
+        val rotCentered = rotMatrix * DenseVector(pt(0) - centre(0), pt(1) - centre(1)).map(_.toDouble)
+        CoordVector2D((rotCentered(0) + centre(0)).toFloat, (rotCentered(1) + centre(1)).toFloat)
+
+      }
+      def takeDerivative(x: Point2D) = {
+        rotMatrix.map(_.toFloat)
+      }
+    }
+  }
+
+  def takeDerivativeWRTParameters(p: ParameterVector) = { x: Point2D =>
+    val sa = math.sin(p(0))
+    val ca = math.cos(p(0))
+    val cx = centre(0)
+    val cy = centre(1)
+
+    DenseMatrix(
+      (-sa * (x(0) - cx) - ca * (x(1) - cy)).toFloat,
+      (ca * (x(0) - cx) - sa * (x(1) - cy)).toFloat)
+  }
+}
 
 object TransformationSpace {
   type ParameterVector = DenseVector[Float]
--- src/test/scala/smptk/registration/TransformationTests.scala
+++ src/test/scala/smptk/registration/TransformationTests.scala
@@ -13,31 +13,96 @@ import smptk.image.Resample
 import smptk.image.DiscreteImageDomain1D
 import smptk.image.ContinuousScalarImage1D
 import smptk.image.Geometry.CoordVector1D
+import org.scalatest.matchers.ShouldMatchers
+import smptk.image.Geometry.CoordVector2D
 
-class ImageTest extends FunSpec {
+class ImageTest extends FunSpec with ShouldMatchers {
+
+  describe("A Rotation in 2D") {
+    it("Rotates a point correctly") {
+      val center = CoordVector2D(2f, 3.5f)
+      val rs = RotationSpace2D(center)
+      val phi = scala.math.Pi / 2
+      val rotationParams = rs.rotationParametersToParameterVector(phi.toFloat)
+      val rotate = rs(rotationParams)
+      val pt = CoordVector2D(2f, 2f)
+      val rotatedPt = rotate(pt)
+
+      (rotatedPt(0)) should be(3.5f plusOrMinus 0.0001f)
+      (rotatedPt(1)) should be(3.5f plusOrMinus 0.0001f)
+    }
+
+  }
+
   describe("A translation in 2D") {
-    it("translates an image") {
+    ignore("translates an image") {
       val discreteImage = ImageIO.read2DScalarImage[Short](new File("/tmp/test.h5")).get
       val continuousImage = Interpolation.interpolate2D(3)(discreteImage)
 
-      val translation = TranslationSpace2D()(DenseVector[Float](10,0))
+      val translation = TranslationSpace2D()(DenseVector[Float](10, 0))
       val translatedImg = continuousImage.compose(translation)
-      val resampledImage = Resample.sample2D[Short](translatedImg, discreteImage.domain, 0)      
+      val resampledImage = Resample.sample2D[Short](translatedImg, discreteImage.domain, 0)
       ImageIO.writeImage(resampledImage, new File("/tmp/resampled.h5"))
 
     }
 
+    describe("composed with a rotation") {
+
+      val ts = TranslationSpace2D()
+      val center = CoordVector2D(2f, 3.5f)
+      val rs = RotationSpace2D(center)
+
+      val productSpace = ts.product(rs)
+
+      it("can be composed with a rotation 2D") {
+        assert(productSpace.parametersDimensionality === ts.parametersDimensionality + rs.parametersDimensionality)
+      }
+
+      val transParams = DenseVector(1.f, 1.5f)
+      val translate = ts(transParams.map(_.toFloat))
+
+      val phi = scala.math.Pi / 2
+      val rotationParams = rs.rotationParametersToParameterVector(phi.toFloat)
+      val rotate = rs(rotationParams)
+
+      val pt = CoordVector2D(2f, 2f)
+      val rotatedPt = rotate(pt)
+      println("rotated point " + rotatedPt)
+      val translatedRotatedPt = translate(rotatedPt)
+
+      val productParams = DenseVector.vertcat(transParams, rotationParams)
+      val productTransform = productSpace(productParams)
+
+      it("correctly transforms a point") {
+        assert(productTransform(pt) === translatedRotatedPt)
+      }
+      val productDerivative = (x: CoordVector2D[Float]) => 
+        breeze.linalg.DenseMatrix.horzcat(
+            ts.takeDerivativeWRTParameters(transParams)(x), 
+            (rs.takeDerivativeWRTParameters(rotationParams)(x))
+        )
+      it("differentiates correctly with regard to parameters") {
+        assert(productSpace.takeDerivativeWRTParameters(productParams)(pt) === productDerivative(pt))
+      }
+      it("differenetiates correctly the parametrized transforms") {
+        assert(productTransform.takeDerivative(pt) === translate.takeDerivative(rotate(pt)) * rotate.takeDerivative(pt))
+      }
+    }
+
     it("translates a 1D image") {
-      val domain = DiscreteImageDomain1D(-50, 1, 100)  
-      val continuousImage = ContinuousScalarImage1D(domain.isInside, (x : CoordVector1D[Float]) => x * x, (x : CoordVector1D[Float]) => DenseVector(2 * x ))
+      val domain = DiscreteImageDomain1D(-50, 1, 100)
+      val continuousImage = ContinuousScalarImage1D(domain.isInside, (x: CoordVector1D[Float]) => x * x, (x: CoordVector1D[Float]) => DenseVector(2 * x))
 
       val translation = TranslationSpace1D()(DenseVector[Float](10))
       val translatedImg = continuousImage.compose(translation)
-      
 
       assert(translatedImg(-10) === 0)
     }
 
-  
   }
+
+  describe("A Transformation space") {
+
+  }
+
 }
