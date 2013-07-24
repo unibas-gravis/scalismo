801eb3c515e94aa14af6bcdbf8d47b5d05979248
Marcel Luethi <marcel.luethi@unibas.ch>
2013-07-24 10:59:44 +0200

    added initialization method for package smptk
    The initialization method is called before smptk is used and loads the
    native libraries.

---
+++ src/main/scala/smptk/package.scala
@@ -0,0 +1,13 @@
+import ch.unibas.gravis.nativelib.NativeLibraryBundles
+package object smptk {
+
+  var initialized = false
+
+  def initialize(): Unit = {
+    if (initialized == false) {
+      NativeLibraryBundles.initialize(NativeLibraryBundles.InitializationMode.WARNONFAIL)
+      initialized = true
+    }
+
+  }
+}
