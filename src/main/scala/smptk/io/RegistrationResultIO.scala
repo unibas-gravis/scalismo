d865fc95e47c80742fda05b1139e55d2d4bdb3e1
bouabene <ghazi.bouabene@unibas.ch>
2013-08-13 12:08:34 +0200

    rebase with master

---
+++ src/main/scala/smptk/io/RegistrationResultIO.scala
@@ -0,0 +1,43 @@
+package smptk.io
+
+import play.api.libs.json._
+import play.api.libs.functional._
+import play.api.libs.functional.syntax._
+import breeze.linalg.DenseVector
+import play.api.data.validation.ValidationError
+import play.api.libs.json.Writes._
+import smptk.registration.RegistrationResult
+import smptk.geometry.TwoD
+import smptk.registration.TransformationSpace.ParameterVector
+import smptk.geometry.ThreeD
+import com.twitter.util.Eval
+import smptk.registration.RegistrationConfiguration
+import java.io.File
+
+object RegistrationResultsIO {
+
+  //def notEqualReads[Array[Double]](v: Int)(implicit r: Reads[Array[Double]]): Reads[Array[Double]] = Reads.filterNot(ValidationError("validate.error.unexpected.value", v))( p=> p == v )
+  implicit val parameterVectorRead = ((__ \ "parameterVector").read[Array[Float]]).map(DenseVector.apply _)
+
+  implicit object parameterVectorWrite extends Writes[ParameterVector] {
+    def writes(m: ParameterVector) = JsObject(List("parameterVector" -> Json.toJson(m.toArray)))
+  }
+  
+  implicit object regResult2DWrite extends Writes[RegistrationResult[TwoD]] {
+    def writes(m: RegistrationResult[TwoD]) = JsObject(List("TransformParameters" -> Json.toJson(m.parameters), "configFileName" -> Json.toJson(m.configurationFileName) ))
+  }
+  
+  implicit object regResult3DWrite extends Writes[RegistrationResult[ThreeD]] {
+    def writes(m: RegistrationResult[ThreeD]) = JsObject(List("TransformParameters" -> Json.toJson(m.parameters), "configFileName" -> Json.toJson(m.configurationFileName) ))
+  }
+  
+  implicit val regResult2DRead =((__ \ "TransformParameters").read[ParameterVector] and (__ \ "configFileName").read[String])( (params, fileName) => {
+    val config = Eval[RegistrationConfiguration[TwoD]](new File(fileName)) // this could be very dangerous since implicit execution of config code
+    RegistrationResult(config.transformationSpace(params), params)
+  })
+  
+  implicit val regResult3DRead =((__ \ "TransformParameters").read[ParameterVector] and (__ \ "configFileName").read[String])( (params, fileName) => {
+    val config = Eval[RegistrationConfiguration[ThreeD]](new File(fileName)) // this could be very dangerous since implicit execution of config code
+    RegistrationResult(config.transformationSpace(params), params, Some(fileName))
+  })
+}
