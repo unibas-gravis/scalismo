e6b12c61a201584760233b894ef74a045c42293d
Marcel Luethi <marcel.luethi@unibas.ch>
2013-08-13 08:11:02 +0200

    added method for creating a MatrixNxN to dimTraits

--- src/main/scala/smptk/geometry/package.scala
+++ src/main/scala/smptk/geometry/package.scala
@@ -5,6 +5,7 @@ package object geometry {
     def dimensionality: Int
     def zeroVector: Vector[D]
     def createVector(d: Array[Float]): Vector[D]
+    def createMatrixNxN(d : Array[Float]) : MatrixNxN[D]
   }
 
   implicit val oneD = new DimTraits[OneD] {
@@ -15,6 +16,14 @@ package object geometry {
         throw new Exception(s"Require array of size 1 to create a Vector1D (got ${d.size}")
       Vector1D(d(0))
     }
+
+    def createMatrixNxN(d: Array[Float]) = {
+      if (d.size != 1) {
+        throw new Exception(s"Require array of size 1 to create a Matrix1x1 (got ${d.size}")
+      }
+      Matrix1x1(d)
+    }
+
   }
   implicit val twoD = new DimTraits[TwoD] {
     def dimensionality = 2
@@ -24,6 +33,13 @@ package object geometry {
         throw new Exception(s"Require array of size 1 to create a Vector1D (got ${d.size}")
       Vector2D(d(0), d(1))
     }
+    def createMatrixNxN(d: Array[Float]) = {
+      if (d.size != 4) {
+        throw new Exception(s"Require array of size 4 to create a Matrix2x2 (got ${d.size}")
+      }
+      Matrix2x2(d)
+    }
+
   }
   implicit val threeD = new DimTraits[ThreeD] {
     def dimensionality = 3
@@ -33,5 +49,12 @@ package object geometry {
         throw new Exception(s"Require array of size 1 to create a Vector3D (got ${d.size}")
       Vector3D(d(0), d(1), d(2))
     }
+    def createMatrixNxN(d: Array[Float]) = {
+      if (d.size != 9) {
+        throw new Exception(s"Require array of size 9 to create a Matrix3x3 (got ${d.size}")
+      }
+      Matrix3x3(d)
+    }
+
   }
 }
