37446982175ff593c51eb771eb677ef008f851b6
Marcel Luethi <marcel.luethi@unibas.ch>
2013-08-16 17:13:23 +0200

    fixed tests after change of pixelValues in Image from IndexedSeq to Array

--- src/test/scala/smptk/filters/DistanceTransformTests.scala
+++ src/test/scala/smptk/filters/DistanceTransformTests.scala
@@ -18,17 +18,17 @@ class DistanceTransformTests extends FunSpec with ShouldMatchers {
   describe("A 1D distance transform") {
     it("yields the right distance values") {
       val dom1D = DiscreteImageDomain1D(Point1D(0.0), Vector1D(1.0), Index1D(5))
-      val img1D = DiscreteScalarImage1D(dom1D, IndexedSeq(1.0, 1.0, 0.0, 1.0, 1.0))
+      val img1D = DiscreteScalarImage1D(dom1D, Array(1.0, 1.0, 0.0, 1.0, 1.0))
       
       val dm =  DistanceTransform.euclideanDistanceTransform1D(img1D)
-      dm.pixelValues should equal(IndexedSeq(2.0, 1.0, 0.0, 1.0, 2.0))     
+      dm.pixelValues should equal(Array(2.0, 1.0, 0.0, 1.0, 2.0))     
     }
   }
 
     describe("A 2D distance transform") {
     it("yields the right distance values") {
       val dom2D = DiscreteImageDomain2D(Point2D(0.0, 0.0), Vector2D(1.0, 1.0), Index2D(4, 3))
-      val img2D = DiscreteScalarImage2D(dom2D, IndexedSeq(1.0, 1.0, 1.0, 1.0, 1. , 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0))
+      val img2D = DiscreteScalarImage2D(dom2D, Array(1.0, 1.0, 1.0, 1.0, 1. , 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0))
       
       val dm =  DistanceTransform.euclideanDistanceTransform2D(img2D)
       println(dm)
