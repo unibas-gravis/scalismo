83eb8707b7beb85c5bdab737cc3e7f9feb39e2e3
Christoph Langguth <christoph.langguth@unibas.ch>
2013-10-24 15:17:55 +0200

    added option to convert Mesh to VTK Polydata using existing template (which is much faster)

--- src/test/scala/org/statismo/stk/core/utils/ConversionTests.scala
+++ src/test/scala/org/statismo/stk/core/utils/ConversionTests.scala
@@ -19,19 +19,25 @@ class ConversionTests extends FunSpec with ShouldMatchers {
     it("can be converted to and from vtk") {
       val path = getClass().getResource("/facemesh.h5").getPath
       val origmesh = MeshIO.readHDF5(new java.io.File(path)).get
-      val vtkpd = MeshConversion.meshToVTKPolyData(origmesh)            
+      val vtkpd = MeshConversion.meshToVTKPolyData(origmesh)
       val restoredMesh = MeshConversion.vtkPolyDataToTriangleMesh(vtkpd).get
       origmesh should equal(restoredMesh)
+
+      // test conversion with template
+      val vtkpd2 = MeshConversion.meshToVTKPolyData(origmesh, Some(vtkpd))
+      val restoredMesh2 = MeshConversion.vtkPolyDataToTriangleMesh(vtkpd2).get
+      origmesh should equal(restoredMesh2)
+
     }
   }
   describe("an 2D image") {
     it("can be converted to and from vtk") {
-      val path = getClass().getResource("/lena.h5").getPath      
+      val path = getClass().getResource("/lena.h5").getPath
       val origimg = ImageIO.read2DScalarImage[Short](new java.io.File(path)).get
-      val vtksp = ImageConversion.image2DTovtkStructuredPoints(origimg)            
+      val vtksp = ImageConversion.image2DTovtkStructuredPoints(origimg)
       val restoredImg = ImageConversion.vtkStructuredPointsTo2DScalarImage[Short](vtksp).get
       origimg should equal(restoredImg)
-      
+
     }
   }
 
