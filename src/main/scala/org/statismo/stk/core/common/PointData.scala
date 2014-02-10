ad5c5ef4f47505378aa3db8587229928a40bd60b
Marcel Luethi <marcel.luethi@unibas.ch>
2014-02-10 18:30:06 +0100

    Implementation of a first version of Active Shape Models.
    The code still lacks a mechanism to deal with outlier points.

--- src/main/scala/org/statismo/stk/core/common/PointData.scala
+++ src/main/scala/org/statismo/stk/core/common/PointData.scala
@@ -3,25 +3,32 @@ package org.statismo.stk.core.common
 import org.statismo.stk.core.geometry.Dim
 import scala.reflect.ClassTag
 
-
-trait PointData[D <: Dim, @specialized(Float, Short) Pixel] extends PartialFunction[Int, Pixel] {
+trait PointData[D <: Dim, A] extends PartialFunction[Int, A] {
   def domain: DiscreteDomain[D]
-  def values: Array[Pixel]
-  def valueDimensionality: Int
-  def apply(idx: Int): Pixel = values(idx)
+  def values: Array[A]
+
+ 
+  def apply(idx: Int): A = values(idx)
   def isDefinedAt(idx: Int) = idx >= 0 && idx <= values.size
 
-  def foreach[A](f: Pixel => A): Unit = values.foreach(f)
-  def map[Pixel2: ScalarValue: ClassTag](f: Pixel => Pixel2): PointData[D, Pixel2]
+  def foreach(f: A => A): Unit = values.foreach(f)
+  
 
   override def hashCode = values.deep.hashCode
   override def equals(other: Any): Boolean = other match {
-    case that: PointData[D, Pixel] => {
+    case that: PointData[D, A] => {
       that.canEqual(this) && this.values.deep == that.values.deep && this.domain == that.domain
     }
     case _ => false
   }
 
-  def canEqual(other: Any): Boolean = other.isInstanceOf[PointData[D, Pixel]]
+  def canEqual(other: Any): Boolean = other.isInstanceOf[PointData[D, A]]
 }
+
+trait ScalarPointData[D <: Dim, @specialized(Float, Short) Pixel] extends PointData[D, Pixel] {
+  def valueDimensionality : Int
+  def map[Pixel2: ScalarValue: ClassTag](f: Pixel => Pixel2): ScalarPointData[D, Pixel2]
+
+}
+
 
