ca16f728a39794418e892b54e06b75df4526aa0e
Marcel Luethi <marcel.luethi@unibas.ch>
2014-03-07 14:42:48 +0100

    Refactoring of Discrete domain concept.
    Introduced notion of UnstructuredPointsDomain, which is now the baseclass of triangel mesh.

--- src/main/scala/org/statismo/stk/core/common/DiscreteDomain.scala
+++ src/main/scala/org/statismo/stk/core/common/DiscreteDomain.scala
@@ -2,7 +2,8 @@ package org.statismo.stk.core
 package common
 
 import scala.collection.SeqView
-import geometry.{Point, Vector, Dim, OneD, TwoD, ThreeD}
+import org.statismo.stk.core.geometry._
+import org.statismo.stk.core.mesh.kdtree.KDTreeMap
 
 
 trait Cell { 
@@ -10,11 +11,45 @@ trait Cell {
 }
 
 trait DiscreteDomain[D <: Dim] extends Domain[D]{
-  def dimensionality: Int
+
+
+  def dimTraits : DimTraits[D]
+
+  def dimensionality: Int = dimTraits.dimensionality
+
   def points: SeqView[Point[D],Seq[_]]
   // def cells : IndexedSeq[Cell] // TODO add it later here
-  def numberOfPoints: Int
+
+  def numberOfPoints: Int = points.size
 
   //def neighbors(pt: Dim): IndexedSeq[Dim]
   def isDefinedAt(i: Int) = i >= 0 && i <= points.size
+
+
+
+}
+
+
+class UnstructuredPointsDomain[D <: Dim : DimTraits](_points : IndexedSeq[Point[D]]) extends DiscreteDomain[D] {
+
+  override val dimTraits =  implicitly[DimTraits[D]]
+  private[this] val kdTreeMap = KDTreeMap.fromSeq(points.zipWithIndex.toIndexedSeq)
+  override def points = _points.view
+
+
+  def isDefinedAt(pt: Point[D]) = {
+    val (closestPt, _) = findClosestPoint(pt)
+    closestPt == pt
+  }
+
+
+  def findClosestPoint(pt: Point[D]): (Point[D], Int) = {
+    val nearestPtsAndIndices = (kdTreeMap.findNearest(pt, n = 1))
+    nearestPtsAndIndices(0)
+  }
+
+  def findNClosestPoints(pt: Point[D], n: Int): Seq[(Point[D], Int)] = {
+    kdTreeMap.findNearest(pt, n)
+  }
+
 }
