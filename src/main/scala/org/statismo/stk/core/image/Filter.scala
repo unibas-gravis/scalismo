8395f7d8d15ee534fd3dddc8465999edc293ff44
bouabene <ghazi.bouabene@unibas.ch>
2014-04-07 17:41:47 +0200

    simplified convolution interface

--- src/main/scala/org/statismo/stk/core/image/Filter.scala
+++ src/main/scala/org/statismo/stk/core/image/Filter.scala
@@ -25,7 +25,7 @@ case class GaussianFilter1D(stddev: Double) extends Filter[OneD] {
 
 case class GaussianFilter2D(stddev: Double) extends Filter[TwoD] {
   def apply(p: Point[TwoD]) = {
-    val v = (Math.exp(-((p(0) * p(0) + p(1) * p(1)) / (2 * stddev * stddev)))) / (Math.PI * 2 * stddev * stddev)
+    val v = Math.exp(-((p(0) * p(0) + p(1) * p(1)) / (2 * stddev * stddev))) / (Math.PI * 2 * stddev * stddev)
     v.toFloat
   }
 
@@ -33,15 +33,13 @@ case class GaussianFilter2D(stddev: Double) extends Filter[TwoD] {
   def support = BoxedDomain2D(Point2D(-extent, -extent), Point2D(extent, extent))
 }
 
-
-
 case class GaussianFilter3D(stddev: Double) extends Filter[ThreeD] {
   def apply(p: Point[ThreeD]) = {
-    val stddev2 =  stddev * stddev 
-    val stddev6 =  stddev2 * stddev2 * stddev2
+    val stddev2 = stddev * stddev
+    val stddev6 = stddev2 * stddev2 * stddev2
 
-    val PI3= Math.PI *Math.PI *Math.PI 
-    val v = Math.exp(-((p(0) * p(0) + p(1) * p(1)+  p(2) * p(2)) / (2 * stddev2)))  / Math.sqrt(8*PI3*stddev6)
+    val PI3 = Math.PI * Math.PI * Math.PI
+    val v = Math.exp(-((p(0) * p(0) + p(1) * p(1) + p(2) * p(2)) / (2 * stddev2))) / Math.sqrt(8 * PI3 * stddev6)
     v.toFloat
   }
 
@@ -49,17 +47,24 @@ case class GaussianFilter3D(stddev: Double) extends Filter[ThreeD] {
   def support = BoxedDomain3D(Point3D(-extent, -extent, -extent), Point3D(extent, extent, extent))
 }
 
-case class BoxedFilter1D() extends Filter[OneD] {
-  def apply(p: Point[OneD]) = 1f
-  def support = BoxedDomain1D(Point1D(-0.5f), Point1D(0.5f))
+case class BoxedFilter1D(width: Int) extends Filter[OneD] {
+  val w = width / 2f
+  def apply(p: Point[OneD]) = {
+    if (p(0) < w && p(0) > -w) width 
+    else if( p(0).abs == w) w
+    else 0f
+  }
+  def support = BoxedDomain1D(Point1D(-w), Point1D(w))
 }
 
-case class BoxedFilter2D() extends Filter[TwoD] {
-  def apply(p: Point[TwoD]) = 1f
-  def support = BoxedDomain2D(Point2D(-0.5f, -0.5f), Point2D(0.5f,0.5f))
+case class BoxedFilter2D(width: Int) extends Filter[TwoD] {
+  def apply(p: Point[TwoD]) = if (support.isDefinedAt(p)) 1f else 0f
+  val w = width / 2f
+  def support = BoxedDomain2D(Point2D(-w, -w), Point2D(w, w))
 }
 
-case class BoxedFilter3D() extends Filter[ThreeD] {
-  def apply(p: Point[ThreeD]) = 1f
-  def support = BoxedDomain3D(Point3D(-0.5f, -0.5f, -0.5f), Point3D(0.5f,0.5f,0.5f))
+case class BoxedFilter3D(width: Int) extends Filter[ThreeD] {
+  def apply(p: Point[ThreeD]) = if (support.isDefinedAt(p)) 1f else 0f
+  val w = width / 2f
+  def support = BoxedDomain3D(Point3D(-w, -w, -w), Point3D(w, w, w))
 }
--- src/test/scala/org/statismo/stk/core/image/SmoothingTest.scala
+++ src/test/scala/org/statismo/stk/core/image/SmoothingTest.scala
@@ -1,6 +1,5 @@
 //package org.statismo.stk.core.image
 //
-//import Image._
 //import org.scalatest.FunSpec
 //import org.scalatest.matchers.ShouldMatchers
 //import org.statismo.stk.core.image.Interpolation._
@@ -21,72 +20,65 @@
 //import org.statismo.stk.core.common.BoxedDomain1D
 //import org.statismo.stk.core.common.RealSpace1D
 //import org.statismo.stk.core.common.BoxedDomain2D
-//
-//// A lot of the tests below are meant to be evaluated visually and are therefore ignored
+//import org.statismo.stk.core.utils.Visualization._
+//import org.statismo.stk.core.common.BoxedDomain
 //
 //class SmoothingTest extends FunSpec with ShouldMatchers with PrivateMethodTester {
 //
+//  org.statismo.stk.core.initialize
+//
 //  describe("A 1D function smoothing") {
 //
-//    val distr = breeze.stats.distributions.Gaussian(0., 0.2)
 //    val domain = DiscreteImageDomain1D(-100., 0.1, 2000)
-//    val discreteImage = DiscreteScalarImage1D(domain, domain.points.map(x => if (math.round(x(0)) % 10 == 0) -1. else 1.).toIndexedSeq)
-//    val continuousImg = Interpolation.interpolate(discreteImage, 0)
-//
-//    def noisySine(x: Point[OneD]): Double = {
+//   
+//    def noisySine(x: Point[OneD]): Float = {
 //      //continuousImg(x)
-//      Math.sin(x(0)) + breeze.stats.distributions.Gaussian(0., 0.1).draw
+//      (Math.sin(x(0)) + breeze.stats.distributions.Gaussian(0., 0.4).draw).toFloat
 //
 //    }
 //
 //    val noisyImage = ContinuousScalarImage1D(BoxedDomain1D(-5.0, 5.0), noisySine)
-//    val discreteDomain = DiscreteImageDomain1D(-5., 0.1, 100)
-//    
-//  
-//    ignore("Works with a box filter (via convolution)") {
-//      //Utils.show1D(noisyImage, discreteDomain)
-//      val boxFilter = BoxedFilter1D()
-//      val boxedregion = BoxedDomain1D(-0.5, 0.5)
-//      val integrator = Integrator[OneD](IntegratorConfiguration(UniformSampler1D(boxedregion), 100))
+//    //val discreteDomain = DiscreteImageDomain1D(-5., 0.1, 100)
+//    val box = BoxedDomain1D(-5.0, 5.0)
 //
-//      val smoothed = noisyImage.convolve(boxFilter, integrator)
-//      //Utils.show1D(smoothed, discreteDomain)
+//    ignore("Works with a box filter (via convolution)") {
+//      show(noisyImage, box, -1.0)
+//      val boxFilter = BoxedFilter1D(1)
+//      val smoothed = noisyImage.convolve(boxFilter,100)
+//      show(smoothed, box, -1.0)
 //    }
 //
-//    it("Works with Gaussian filter (via Utils method)") {
+//    ignore("Works with Gaussian filter") {
+//      val gaussianFilter = GaussianFilter1D(1)
+//      val gaussianSmoothed = noisyImage.convolve(gaussianFilter,100)
+//      show(gaussianSmoothed, box, -1.0)
 //
-//      val integrator = Integrator[OneD](IntegratorConfiguration(UniformSampler1D(boxedregion), 5000))
-//      val gaussianSmoothed = Utils.gaussianSmoothing1D(noisyImage, 0.01, integrator)
-//      //Utils.show1D(gaussianSmoothed, discreteDomain)
-//
-//      val gaussianSmoothed2 = Utils.gaussianSmoothing1D(noisyImage, 2., integrator)
-//      //Utils.show1D(gaussianSmoothed2, discreteDomain)
-//
-//      val p = Point1D(0.5)
-//      gaussianSmoothed(p) should be(Math.sin(p(0)) plusOrMinus 0.01)
 //    }
 //
 //  }
 //
 //  describe("A 1D convolution ") {
 //
-//    it("gives the b-spline basis function when convolving a box with a box filter") {
+//    ignore("gives the b-spline basis function when convolving a box with a box filter") {
 //
-//      val boxImage = ContinuousScalarImage1D(RealSpace1D, (p: Point[OneD]) => if (p(0) >= -0.5 && p(0) <= 0.5) 1. else 0.)
-//      val filter = BoxedFilter1D()
+//      val boxImage = ContinuousScalarImage1D(RealSpace1D, (p: Point[OneD]) => if (p(0) >= -0.5 && p(0) <= 0.5) 1f else 0f)
+//      val filter = BoxedFilter1D(1)
 //
-//      val integrator = Integrator[OneD](IntegratorConfiguration(UniformSampler1D(), 100))
+//      val discreteDomain = DiscreteImageDomain1D(Point1D(-5), Vector1D(0.01f), Index1D(1000))
 //
-//      val convoledOnce = boxImage.convolve(filter, integrator)
-//      val convolvedTwice = convoledOnce.convolve(filter, integrator)
-//      val convolvedThreeTimes = convolvedTwice.convolve(filter, integrator)
+//      val convoledOnce = boxImage.convolve(filter, 100)
+//      val convolvedTwice = convoledOnce.convolve(filter,100)
+//      val cv2 = Resample.sample[Float](convolvedTwice, discreteDomain, -1f)
+//      val convolvedThreeTimes = Interpolation.interpolate(cv2, 3).convolve(filter,100)
+//     
+//      val box = BoxedDomain1D(-5, 5)
+//      show(Resample.sample[Float](convoledOnce, discreteDomain, -1f))
+//      show(cv2)
+//      show(Resample.sample[Float](convolvedThreeTimes, discreteDomain, -1f))
 //
-//      val domain = DiscreteImageDomain1D(-5., 0.1, 100)
-//      //Utils.show1D(convoledOnce, domain)
-//
-//      convoledOnce(0.) should be(Interpolation.bSpline(1)(0.) plusOrMinus (0.1))
-//      convolvedTwice(0.) should be(Interpolation.bSpline(2)(0.) plusOrMinus (0.1))
-//      convolvedThreeTimes(0.) should be(Interpolation.bSpline(3)(0.) plusOrMinus (0.1))
+//      convoledOnce(0f) should be(Interpolation.bSpline(1)(0.).toFloat plusOrMinus (0.1f))
+//      convolvedTwice(0f) should be(Interpolation.bSpline(2)(0.).toFloat plusOrMinus (0.1f))
+//      convolvedThreeTimes(0f) should be(Interpolation.bSpline(3)(0.).toFloat plusOrMinus (0.1f))
 //
 //    }
 //
@@ -99,45 +91,42 @@
 //      def domainBox(origin: Point[TwoD], extent: Point[TwoD]) =
 //        (p: Point[TwoD]) => (p(0) >= origin(0) && p(1) >= origin(1) && p(0) <= extent(0) && p(1) <= extent(1))
 //
-//      def innerBox(origin: Point2D, extent: Point2D) = (p: Point[TwoD]) => if (domainBox(origin, extent)(p)) 2. else 1.
+//      def innerBox(origin: Point2D, extent: Point2D) = (p: Point[TwoD]) => if (domainBox(origin, extent)(p)) 200f else 1f
 //
-//      val boxImage = ContinuousScalarImage2D(BoxedDomain2D((-5., -5.), (5., 5.)), innerBox((-2., -2.), (3., 2.)))
+//      val boxImage = ContinuousScalarImage2D(BoxedDomain2D((-5., -5.), (5., 5.)), innerBox((-2., -2.), (2., 2.)))
 //
-//      val domain = DiscreteImageDomain2D((-6., -6.), (0.1, 0.1), (120, 120))
+//      val box = BoxedDomain2D(Point2D(-6f, -6f), Point2D(6f, 6f))
 //
-//      //Utils.show2D(boxImage, domain)
+//      show(boxImage, box, -1f)
 //
-//      val integrator = Integrator[TwoD](IntegratorConfiguration(UniformSampler2D(), 5000))
-//      val filter = GaussianFilter2D(5.)
-//      val smoothed = boxImage.convolve(filter, integrator)
-//
-//      //Utils.show2D(smoothed, domain)
+//      val filter = GaussianFilter2D(1.)
+//      val smoothed = boxImage.convolve(filter, 400)
+//      show(smoothed, box, -1f)
 //
 //    }
 //
 //    val discreteImage = ImageIO.read2DScalarImage[Short](new File(getClass.getResource("/lena256.h5").getPath())).get
+//    
+//    println("Lena's domain : " + discreteImage.domain)
 //    val originalImage = Interpolation.interpolate(discreteImage, 3)
-//    //Utils.show2D(originalImage, discreteImage.domain)
+//    val f = (p:Point[TwoD]) => if(p.toVector.norm < 5) 250f else originalImage(p)
+//    val dummyImage = ContinuousScalarImage2D(discreteImage.domain, f,  None)
+//    show(dummyImage, BoxedDomain2D(discreteImage.domain.origin, discreteImage.domain.extent), -1f)
 //
-//    ignore("works with box filter") {
-//      val boxThresh = 50.
-//      val boxFilter = BoxedFilter2D()
+//    it("works with box filter") {
+//      val boxFilter = GaussianFilter2D(6.)
 //
-//      val filterSupport = DiscreteImageDomain2D((-1., -1.), (0.1, 0.1), (100, 100))
-//
-//      val integrator = Integrator[TwoD](IntegratorConfiguration(UniformSampler2D(), 100))
-//      val smoothed = originalImage.convolve(boxFilter, integrator)
-//      //Utils.show2D(smoothed, discreteImage.domain)
+//      val smoothed = originalImage.convolve(boxFilter,400)
+//      show(smoothed, BoxedDomain2D(discreteImage.domain.origin, discreteImage.domain.extent), -1f)
 //    }
 //
 //    ignore("works with Gaussian filter") {
 //
-//      val integrator = Integrator[TwoD](IntegratorConfiguration(UniformSampler2D(), 500))
 //      val deviations = List(8., 6., 4., 2., 1.)
 //
 //      for (d <- deviations) {
-//        val smoothed = Utils.gaussianSmoothing2D(originalImage, d, integrator)
-//       // Utils.show2D(smoothed, discreteImage.domain)
+//        val smoothed = originalImage.convolve(GaussianFilter2D(d), 100)
+//        show(smoothed, BoxedDomain2D(discreteImage.domain.origin, discreteImage.domain.extent), -1f)
 //      }
 //    }
 //
