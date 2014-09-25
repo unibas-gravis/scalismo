bab9e1d411d014be05e83d536cbd83f1e9ac299c
Christoph Langguth <christoph.langguth@unibas.ch>
2014-09-25 15:36:23 +0200

    Changed RandomSVDTest to minimize failure probability

--- src/test/scala/org/statismo/stk/core/numerics/RandomSVDTest.scala
+++ src/test/scala/org/statismo/stk/core/numerics/RandomSVDTest.scala
@@ -2,12 +2,13 @@ package org.statismo.stk.core.numerics
 
 import org.scalatest.FunSpec
 import org.scalatest.matchers.ShouldMatchers
-import org.statismo.stk.core.kernels._
 import org.statismo.stk.core.geometry._
+import org.statismo.stk.core.kernels._
 
 class RandomSVDTest extends FunSpec with ShouldMatchers {
 
   describe("The random svd") {
+
     it("accurately approximates the first 10 eigenvectors and eigenvalues of a gaussian kernel matrix") {
       val k = UncorrelatedKernel1x1(GaussianKernel1D(10))
       val xs = (0 until 500).map(x => Point1D(x))
@@ -16,17 +17,12 @@ class RandomSVDTest extends FunSpec with ShouldMatchers {
       val (ur, lr, vrt) = RandomSVD.computeSVD(Kdouble, 10)
       val (ub, lb, vbt) = breeze.linalg.svd(Kdouble)
 
-      for (j <- 0 until 10) {
-        (lr(j) should be(lb(j) plusOrMinus 0.005))
+      val mr = ur(::, 0 until 10) * breeze.linalg.diag(lr(0 until 10)) * vrt(0 until 10, 0 until 10)
+      val mb = ub(::, 0 until 10) * breeze.linalg.diag(lb(0 until 10)) * vbt(0 until 10, 0 until 10)
 
-        for (i <- (0 until ur.rows)) {
-          (math.abs(ur(i, j)) should be(math.abs(ub(i, j)) plusOrMinus 0.005))
-        }
-
-        for (i <- (0 until vrt.rows)) {
-          (math.abs(vrt(i, j)) should be(math.abs(vbt(i, j)) plusOrMinus 0.005))
-        }
-
+      for (j <- 0 until 10; i <- 0 until mr.rows) {
+        val factor = Math.max(1e-5, mr(i, j)) / Math.max(1e-5, mb(i, j))
+        factor should be(1.0 plusOrMinus 0.01)
       }
     }
   }
