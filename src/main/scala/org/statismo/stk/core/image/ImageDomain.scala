b3635a53265cc0ae7c477fe91afb3c4eeaf47a17
bouabene <ghazi.bouabene@unibas.ch>
2014-10-07 13:54:37 +0200

    used implicit creator object to avoid pattern matching on dimension

--- src/main/scala/org/statismo/stk/core/image/ImageDomain.scala
+++ src/main/scala/org/statismo/stk/core/image/ImageDomain.scala
@@ -7,7 +7,11 @@ import org.statismo.stk.core.common.FiniteDiscreteDomain
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
 import breeze.linalg.DenseVector
 
-case class DiscreteImageDomain[D <: Dim: DimOps](origin: Point[D], spacing: Vector[D], size: Index[D]) extends FiniteDiscreteDomain[D] with BoxedDomain[D] {
+abstract class DiscreteImageDomain[D <: Dim: DimOps] extends FiniteDiscreteDomain[D] with BoxedDomain[D] {
+
+  val origin: Point[D]
+  val spacing: Vector[D]
+  val size: Index[D]
 
   override def extent: Point[D] = origin + Vector((spacing.toBreezeVector :* size.toBreezeVector.map(_.toFloat)).data)
 
@@ -15,39 +19,60 @@ case class DiscreteImageDomain[D <: Dim: DimOps](origin: Point[D], spacing: Vect
 
   override def numberOfPoints = (0 until size.dimensionality).foldLeft(1)((res, d) => res * size(d))
 
-  private val _points = size.dimensionality match {
-    case 1 => for (i <- (0 until size(0))) yield Point(origin(0) + spacing(0) * i)
-    case 2 => for (j <- (0 until size(1)); i <- (0 until size(0)))
-      yield Point(origin(0) + spacing(0) * i, origin(1) + spacing(1) * j)
-    case 3 => for (k <- (0 until size(2)); j <- (0 until size(1)); i <- (0 until size(0)))
-      yield Point(origin(0) + spacing(0) * i, origin(1) + spacing(1) * j, origin(2) + spacing(2) * k)
-    case _ => throw new NotImplementedError("Only dimensionality 1, 2 and 3 are supported for DiscreteImageDomain")
-  }
+  override def points: Iterator[Point[D]]
 
-  override def points = _points.toIterator.asInstanceOf[Iterator[Point[D]]]
-  
+  def indexToLinearIndex(idx: Index[D]): Int
 
-  def indexToLinearIndex(idx: Index[D]): Int = idx.dimensionality match {
-    case 1 => idx(0)
-    case 2 => idx(0) + idx(1) * size(0)
-    case 3 => idx(0) + idx(1) * size(0) + idx(2) * size(0) * size(1)
-    case _ => throw new NotImplementedError("Only dimensionality 1, 2 and 3 are supported for DiscreteImageDomain")
+  def linearIndexToIndex(linearIdx: Int): Index[D]
 
-  }
-  def linearIndexToIndex(linearIdx: Int): Index[D] = {
-    val t = size.dimensionality match {
-
-      case 1 => Index[_1D](Array(linearIdx))
-      case 2 => Index[_2D](Array(linearIdx % size(0), linearIdx / size(0)))
-      case 3 => Index[_3D](Array(
-        linearIdx % (size(0) * size(1)) % size(0),
-        linearIdx % (size(0) * size(1)) / size(0),
-        linearIdx / (size(0) * size(1))))
-      case _ => throw new NotImplementedError("Only dimensionality 1, 2 and 3 are supported for DiscreteImageDomain")
-    }
-    t.asInstanceOf[Index[D]]
-  }
 }
 
+private case class DiscreteImageDomain1D(origin: Point[_1D], spacing: Vector[_1D], size: Index[_1D]) extends DiscreteImageDomain[_1D] {
+  private val _points = for (i <- (0 until size(0))) yield Point(origin(0) + spacing(0) * i)
+  override def points: Iterator[Point[_1D]] = _points.toIterator
+  def indexToLinearIndex(idx: Index[_1D]) = idx(0)
+  def linearIndexToIndex(linearIdx: Int) = Index[_1D](Array(linearIdx))
+}
+
+private case class DiscreteImageDomain2D(origin: Point[_2D], spacing: Vector[_2D], size: Index[_2D]) extends DiscreteImageDomain[_2D] {
+  private val _points = for (j <- (0 until size(1)); i <- (0 until size(0))) yield Point(origin(0) + spacing(0) * i, origin(1) + spacing(1) * j)
+  override def points: Iterator[Point[_2D]] = _points.toIterator
+  def indexToLinearIndex(idx: Index[_2D]) = idx(0) + idx(1) * size(0)
+  def linearIndexToIndex(linearIdx: Int) = Index[_2D](Array(linearIdx % size(0), linearIdx / size(0)))
+}
+
+private case class DiscreteImageDomain3D(origin: Point[_3D], spacing: Vector[_3D], size: Index[_3D]) extends DiscreteImageDomain[_3D] {
+  private val _points = for (k <- (0 until size(2)); j <- (0 until size(1)); i <- (0 until size(0)))
+    yield Point(origin(0) + spacing(0) * i, origin(1) + spacing(1) * j, origin(2) + spacing(2) * k)
+  override def points: Iterator[Point[_3D]] = _points.toIterator
+  def indexToLinearIndex(idx: Index[_3D]) = idx(0) + idx(1) * size(0) + idx(2) * size(0) * size(1)
+  def linearIndexToIndex(linearIdx: Int) = Index[_3D](Array(
+    linearIdx % (size(0) * size(1)) % size(0),
+    linearIdx % (size(0) * size(1)) / size(0),
+    linearIdx / (size(0) * size(1))))
+}
+
+object DiscreteImageDomain {
+
+  trait Create[D <: Dim] {
+    def createImageDomain(origin: Point[D], spacing: Vector[D], size: Index[D]): DiscreteImageDomain[D]
+  }
+
+  implicit object createImageDomain2D extends Create[_2D] {
+    override def createImageDomain(origin: Point[_2D], spacing: Vector[_2D], size: Index[_2D]): DiscreteImageDomain[_2D] = new DiscreteImageDomain2D(origin, spacing, size)
+  }
+  
+  implicit object createImageDomain3D extends Create[_3D] {
+    override def createImageDomain(origin: Point[_3D], spacing: Vector[_3D], size: Index[_3D]): DiscreteImageDomain[_3D] = new DiscreteImageDomain3D(origin, spacing, size)
+  } 
+  implicit object createImageDomain1D extends Create[_1D] {
+    override def createImageDomain(origin: Point[_1D], spacing: Vector[_1D], size: Index[_1D]): DiscreteImageDomain[_1D] = new DiscreteImageDomain1D(origin, spacing, size)
+  }
+
+  def apply[D <: Dim](origin: Point[D], spacing: Vector[D], size: Index[D])(implicit evDim: DimOps[D], evCreateRot: Create[D]) = {
+    evCreateRot.createImageDomain(origin, spacing, size)
+  }
+
+}
 
 
