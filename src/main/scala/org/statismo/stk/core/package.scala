5a07b80595c6f2e54d2e4fb25851e62d213b5e7b
Christoph Langguth <christoph.langguth@unibas.ch>
2014-12-18 18:57:09 +0100

    fix compiler warning about declaring class in package object

--- src/main/scala/org/statismo/stk/core/package.scala
+++ src/main/scala/org/statismo/stk/core/package.scala
@@ -4,21 +4,18 @@ import org.statismo.support.nativelibs._
 
 package object core {
 
-  private class Initialization {
-    var done = false
-  }
-
-  private val initialization = new Initialization
+  // this is a hacky way to get an object that can be synchronized on, with a mutable value.
+  private val initialized = Array.fill(1)(false)
 
   /**
    * Initialize and load the required native libraries
    * @param ignoreErrors ignore failures when trying to load libraries. Only set this if you know what you are doing!
    */
-  def initialize(ignoreErrors: Boolean = false) = initialization.synchronized {
-    if (!initialization.done) {
+  def initialize(ignoreErrors: Boolean = false) = initialized.synchronized {
+    if (!initialized(0)) {
       val mode = if (ignoreErrors) NativeLibraryBundles.InitializationMode.WARN_ON_FAIL else NativeLibraryBundles.InitializationMode.TERMINATE_ON_FAIL
       NativeLibraryBundles.initialize(mode)
-      initialization.done = true
+      initialized(0) = true
     }
   }
 }
