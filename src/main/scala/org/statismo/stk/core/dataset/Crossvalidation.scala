2f0195b8dfc3ed98ded872ce12b9555e77a2599c
bouabene <ghazi.bouabene@unibas.ch>
2015-01-28 11:02:25 +0100

    adapted to GP refactoring

--- src/main/scala/org/statismo/stk/core/dataset/Crossvalidation.scala
+++ src/main/scala/org/statismo/stk/core/dataset/Crossvalidation.scala
@@ -10,50 +10,47 @@ import org.statismo.stk.core.statisticalmodel.DiscreteLowRankGaussianProcess
 
 /**
  * Implements utility functions for evaluating the quality of a registered dataset
- * */
+ */
 object Crossvalidation {
 
   type EvaluationFunction[A] = (StatisticalMeshModel, TriangleMesh) => A
 
   private def projectIntoModel(model: StatisticalMeshModel, mesh: TriangleMesh): TriangleMesh = {
     val ptPairs = model.referenceMesh.points.toIndexedSeq.zip(mesh.points.toIndexedSeq)
-    val trainingDeformations = ptPairs.zipWithIndex.map {
-      case ((refPt, targetPt), idx) => (idx, targetPt - refPt)
+
+    val trainingData = ptPairs.zipWithIndex.map {
+      case ((refPt, targetPt), idx) => (idx, targetPt)
     }
-    val posteriorGP = model.regression(model.gp, trainingDeformations, 1e-5, true)
-//    TriangleMesh( model.referenceMesh.point.map())
-    val projectedMesh = model.referenceMesh.warp((pt: Point[_3D]) => pt + posteriorGP.mean(pt))
-    projectedMesh
+    model.project(trainingData, 1e-5)
   }
 
   /**
    * Perform a leave one out crossvalidation. See nFoldCrossvalidation for details
    */
-  def leaveOneOutCrossvalidation[A](dataCollection: DataCollection, evalFun: EvaluationFunction[A], biasModel: Option[DiscreteLowRankGaussianProcess[_3D, _3D]] = None) = {
+  def leaveOneOutCrossvalidation[A](dataCollection: DataCollection, evalFun: EvaluationFunction[A], biasModel: Option[LowRankGaussianProcess[_3D, _3D]] = None) = {
     nFoldCrossvalidation(dataCollection.size, dataCollection, evalFun, biasModel)
   }
 
   /**
    * Perform an n-fold crossvalidation. Given the chosen number of folds, this method will repeatedly split the data collection
-   * into a training and and a test set. 
+   * into a training and and a test set.
    * A [[StatisticalMeshModel]] is then built from the training set of each fold. In case a biasModel is provided, this model is always added to the model built from the training data.
-   * 
+   *
    * For each testing dataset in a fold, the evalFun is called to evaluate the quality of the model built from the training set.
-   * 
-   * @returns a sequence the size of the chosen number of folds that contains the sequence of evaluations for each data item in the fold's testing set, 
+   *
+   * @returns a sequence the size of the chosen number of folds that contains the sequence of evaluations for each data item in the fold's testing set,
    * or an error if the model building for a fold failed.
    */
-  def nFoldCrossvalidation[A](numFolds: Int, dc: DataCollection, evalFun: EvaluationFunction[A], biasModel: Option[LowRankGaussianProcess3D] = None): Seq[Try[Seq[A]]] = {
+  def nFoldCrossvalidation[A](numFolds: Int, dc: DataCollection, evalFun: EvaluationFunction[A], biasModel: Option[LowRankGaussianProcess[_3D, _3D]] = None): Seq[Try[Seq[A]]] = {
 
     val folds = dc.createCrossValidationFolds(numFolds)
     val evalResultsForFolds = for (fold <- folds) yield {
       val td = fold.trainingData
       PCAModel.buildModelFromDataCollection(td).map { pcaModel =>
         val model = if (biasModel.isDefined) PCAModel.augmentModel(pcaModel, biasModel.get) else pcaModel
-        val specializedModel = new StatisticalMeshModel(model.mesh, model.gp.specializeForPoints(model.mesh.points.toIndexedSeq))
         val evalResults = for (testingItem <- fold.testingData.dataItems) yield {
           val testMesh = dc.reference.warp(testingItem.transformation)
-          evalFun(specializedModel, testMesh)
+          evalFun(model, testMesh)
         }
         evalResults
       }
--- src/main/scala/org/statismo/stk/core/dataset/PCAModel.scala
+++ src/main/scala/org/statismo/stk/core/dataset/PCAModel.scala
@@ -16,68 +16,31 @@ import org.statismo.stk.core.statisticalmodel.GaussianProcess
 import org.statismo.stk.core.kernels.MatrixValuedPDKernel
 
 /**
- * Implements utility functions for building a [[StatisticalMeshModel]] from a [[DataCollection]] containing a reference and items in correspondence 
- * */
+ * Implements utility functions for building a [[StatisticalMeshModel]] from a [[DataCollection]] containing a reference and items in correspondence
+ */
 object PCAModel {
 
   type MeshSampler = Sampler[_3D]
 
-  
   /**
    *  Adds a bias model to the given pca model
    */
-  private [dataset] def augmentModel(pcaModel: StatisticalMeshModel, biasModel: LowRankGaussianProcess[_3D, _3D]): StatisticalMeshModel = {
-    
-    val modelGP = pcaModel.gp.interpolate(500)          
-    val newMean = (x: Point[_3D]) => modelGP.mean(x) + biasModel.mean(x)     
+  private[dataset] def augmentModel(pcaModel: StatisticalMeshModel, biasModel: LowRankGaussianProcess[_3D, _3D]): StatisticalMeshModel = {
+
+    val modelGP = pcaModel.gp.interpolate(500)
+    val newMean = (x: Point[_3D]) => modelGP.mean(x) + biasModel.mean(x)
     val newKLBasis = modelGP.klBasis ++ biasModel.klBasis
-    val newGP = new LowRankGaussianProcess(pcaModel.referenceMesh.boundingBox, newMean, newKLBasis)   
-    val newDiscreteGP = newGP.discretize(pcaModel.referenceMesh.points.toIndexedSeq)
-     
-    StatisticalMeshModel(pcaModel.referenceMesh, newDiscreteGP)
-  }
-  
-  /**
-   * A sampler that samples all the points of a mesh
-   */
+    val newGP = new LowRankGaussianProcess(pcaModel.referenceMesh.boundingBox, newMean, newKLBasis)
 
-  private case class DefaultPCAMeshSampler(mesh: TriangleMesh) extends MeshSampler {
-    override val numberOfPoints = mesh.numberOfPoints
-    val p = 1.0 / mesh.area // should be replaced with real mesh volume
-    override val volumeOfSampleRegion = mesh.area
-    override def sample = mesh.points.toIndexedSeq.map(pt => (pt, p))
-  }
-
-  
-  /**
-   * Returns a PCA model with given reference mesh and a set of items in correspondence
-   * 
-   * @param dc DataCollection containing registered dataset
-   * @param sampler A sampler to select points to be considered for computing the PCA 
-   */
-  def buildModelFromDataCollection(dc: DataCollection, sampler: MeshSampler): Try[StatisticalMeshModel] = {
-    buildModelFromTransformations(dc.reference, dc.dataItems.map(_.transformation), Some(sampler))
+    StatisticalMeshModel(pcaModel.referenceMesh, newGP)
   }
 
   /**
-   * Returns a PCA model with given reference mesh and a set of items in correspondence.  
-   * All points of the reference mesh are considered for computing the PCA 
+   * Returns a PCA model with given reference mesh and a set of items in correspondence.
+   * All points of the reference mesh are considered for computing the PCA
    */
   def buildModelFromDataCollection(dc: DataCollection): Try[StatisticalMeshModel] = {
-    buildModelFromTransformations(dc.reference, dc.dataItems.map(_.transformation), None)
+    if (dc.size < 3) return Failure(new Throwable(s"We need to have at least 3 transformations to build a PCA Model (${dc.size} provied"))
+    Success(StatisticalMeshModel.createStatisticalMeshModelFromTransformations(dc.reference, dc.dataItems.map(_.transformation)))
   }
-
-
-  private def buildModelFromTransformations(referenceMesh: TriangleMesh, transformations: Seq[Transformation[_3D]], maybeSampler: Option[MeshSampler]): Try[StatisticalMeshModel] = {
-    if (transformations.size < 3) return Failure(new Throwable(s"We need to have at least 3 transformations to build a PCA Model (${transformations.size} provied"))
-    val sampler = maybeSampler.getOrElse(DefaultPCAMeshSampler(referenceMesh))   
-    val gp = LowRankGaussianProcess.createLowRankGPFromTransformations(referenceMesh.boundingBox, transformations, sampler)
-    Success(new StatisticalMeshModel(referenceMesh, gp))
-  }
-
-
-
-
-
-  
 }
