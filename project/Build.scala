b841fee2c15eff55a2e683af35eec3da63edea47
Christoph Langguth <christoph.langguth@unibas.ch>
2015-03-24 13:15:31 +0100

    Set "default" compile version of scala to 2.10.4
    This avoids accidentally using 2.11 only features if "sbt compile" (only) is used.
    It does not change anything if "sbt +compile" is used.

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ import com.banno.license.Licenses._
 object BuildSettings {
   val buildOrganization = "ch.unibas.cs.gravis"
   val buildVersion = "develop-SNAPSHOT"
-  val buildScalaVersion = "2.11.4"
+  val buildScalaVersion = "2.10.4"
   val publishURL = Resolver.file("file", new File("/export/contrib/statismo/repo/public"))
 
   val buildSettings = Defaults.defaultSettings ++ Seq(
