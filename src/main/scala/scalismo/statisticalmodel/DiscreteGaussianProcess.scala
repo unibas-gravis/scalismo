450c1e2437d511a8b2fb01cfbc8a0832c0768097
Marcel Luethi <marcel.luethi@unibas.ch>
2015-04-10 13:52:57 +0200

    Changed interface of coefficients/projection method for DiscreteGaussianProcess and StatisticalMeshModel

--- src/main/scala/scalismo/statisticalmodel/DiscreteGaussianProcess.scala
+++ src/main/scala/scalismo/statisticalmodel/DiscreteGaussianProcess.scala
@@ -18,9 +18,10 @@ package scalismo.statisticalmodel
 
 import breeze.linalg.{ DenseMatrix, DenseVector }
 import scalismo.common.DiscreteDomain.CanBound
-import scalismo.common.{ VectorField, DiscreteDomain, DiscreteVectorField }
-import scalismo.geometry.{ SquareMatrix, Vector, Dim, NDSpace }
+import scalismo.common.{ RealSpace, VectorField, DiscreteDomain, DiscreteVectorField }
+import scalismo.geometry._
 import scalismo.kernels.{ MatrixValuedPDKernel, Kernel, DiscreteMatrixValuedPDKernel }
+import scalismo.mesh.kdtree.KDTreeMap
 
 /**
  * A representation of a gaussian process, which is only defined on a discrete domain.
@@ -82,11 +83,82 @@ class DiscreteGaussianProcess[D <: Dim: NDSpace: CanBound, DO <: Dim: NDSpace] p
     new DiscreteGaussianProcess(newMean, newDiscreteCov)
   }
 
+  /**
+   * Interpolates discrete Gaussian process to have a new, continuous representation as a [[DiscreteLowRankGaussianProcess]],
+   * using nearest neigbor interpolation (for both mean and covariance function)
+   */
+  def interpolateNearestNeighbor: GaussianProcess[D, DO] = {
+
+    val meanDiscreteGp = this.mean
+    val kdTreeMap = KDTreeMap.fromSeq(domain.pointsWithId.toIndexedSeq)
+
+    def closestPt(pt: Point[D]): (Point[D], Int) = {
+      val closestPts = kdTreeMap.findNearest(pt, n = 1)
+      val (closestPt, closestPtId) = closestPts(0)
+      (closestPt, closestPtId)
+    }
+
+    val newDomain = RealSpace[D]
+    def meanFun(pt: Point[D]): Vector[DO] = {
+      val (_, closestPtId) = closestPt(pt)
+      meanDiscreteGp(closestPtId)
+    }
+
+    val newCov = new MatrixValuedPDKernel[D, DO] {
+      override val domain = newDomain
+      override def k(pt1: Point[D], pt2: Point[D]): SquareMatrix[DO] = {
+        val (_, closestPtId1) = closestPt(pt1)
+        val (_, closestPtId2) = closestPt(pt2)
+        cov(closestPtId1, closestPtId2)
+      }
+    }
+
+    GaussianProcess(VectorField(newDomain, meanFun), newCov)
+
+  }
+
+  /**
+   * Discrete version of [[LowRankGaussianProcess.project(IndexedSeq[(Point[D], Vector[DO])], Double)]]
+   */
+
+  def project(s: DiscreteVectorField[D, DO]): DiscreteVectorField[D, DO] = {
+
+    val sigma2 = 1e-5f // regularization weight to avoid numerical problems
+    val noiseDist = NDimensionalNormalDistribution(Vector.zeros[DO], SquareMatrix.eye[DO] * sigma2)
+    val td = s.values.zipWithIndex.map { case (v, id) => (id, v, noiseDist) }.toIndexedSeq
+    DiscreteGaussianProcess.regression(this, td).mean
+  }
+
 }
 
 object DiscreteGaussianProcess {
 
   def apply[D <: Dim: NDSpace: CanBound, DO <: Dim: NDSpace](mean: DiscreteVectorField[D, DO], cov: DiscreteMatrixValuedPDKernel[D, DO]) = {
     new DiscreteGaussianProcess[D, DO](mean, cov)
+  }
+
+  def apply[D <: Dim: NDSpace: CanBound, DO <: Dim: NDSpace](domain: DiscreteDomain[D], gp: GaussianProcess[D, DO]) = {
+    val domainPoints = domain.points.toIndexedSeq
+
+    val discreteMean = DiscreteVectorField[D, DO](domain, domainPoints.map(pt => gp.mean(pt)))
+
+    val k = (i: Int, j: Int) => gp.cov(domainPoints(i), domainPoints(j))
+    val discreteCov = DiscreteMatrixValuedPDKernel(domain, k)
+
+    new DiscreteGaussianProcess[D, DO](discreteMean, discreteCov)
+  }
+
+  def regression[D <: Dim: NDSpace: CanBound, DO <: Dim: NDSpace](discreteGp: DiscreteGaussianProcess[D, DO],
+    trainingData: IndexedSeq[(Int, Vector[DO], NDimensionalNormalDistribution[DO])]): DiscreteGaussianProcess[D, DO] = {
+
+    // TODO, this is somehow a hack to reuse the code written for the general GP regression. We should think if that has disadvantages
+    // TODO We should think whether we can do it in  a conceptually more clean way.
+
+    val domainPoints = discreteGp.domain.points.toIndexedSeq
+    val gp = discreteGp.interpolateNearestNeighbor
+    val tdForGp = trainingData.map({ case (id, vec, error) => (domainPoints(id), vec, error) })
+    val posterior = GaussianProcess.regression(gp, tdForGp)
+
+    DiscreteGaussianProcess(discreteGp.domain, gp)
   }
 }
