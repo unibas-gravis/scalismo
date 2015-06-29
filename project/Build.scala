79927b75e4e9ba16b5b1d5298f07aa98db8fff0a
Christoph Langguth <christoph.langguth@unibas.ch>
2015-06-29 15:59:03 +0200

    Release version 0.8.0

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ import com.banno.license.Licenses._
 
 object BuildSettings {
   val buildOrganization = "ch.unibas.cs.gravis"
-  val buildVersion = "0.8.0-RC3"
+  val buildVersion = "0.8.0"
   val buildScalaVersion = "2.10.4"
   val publishURL = Resolver.file("file", new File("/export/contrib/statismo/repo/public"))
 
