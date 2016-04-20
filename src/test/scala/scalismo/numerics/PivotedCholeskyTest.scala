fae5609a8339c3afe6bd1426ddedf2be71be52c1
Thomas Gerig <thomas.gerig@unibas.ch>
2016-04-20 16:55:59 +0200

    Added copyright header

--- src/test/scala/scalismo/numerics/PivotedCholeskyTest.scala
+++ src/test/scala/scalismo/numerics/PivotedCholeskyTest.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 University of Basel, Graphics and Vision Research Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package scalismo.numerics
 
 import breeze.linalg.{ DenseMatrix, DenseVector }
@@ -5,10 +21,6 @@ import scalismo.ScalismoTestSuite
 import scalismo.common.BoxDomain3D
 import scalismo.geometry.{ Point, _1D, _3D }
 import scalismo.kernels.{ GaussianKernel, Kernel, UncorrelatedKernel }
-
-/**
- * Created by gerith00 on 19.04.16.
- */
 
 class PivotedCholeskyTest extends ScalismoTestSuite {
 
