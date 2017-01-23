1577029f70efdf26aadb8195ea05f97fe78e93d9
Marcel Luethi <marcel.luethi@unibas.ch>
2017-01-23 13:34:37 +0100

    introduced transform method for Landmarks

--- src/main/scala/scalismo/geometry/Landmark.scala
+++ src/main/scala/scalismo/geometry/Landmark.scala
@@ -15,8 +15,37 @@
  */
 package scalismo.geometry
 
+import breeze.linalg.DenseVector
 import scalismo.io.LandmarkIO
+import scalismo.registration.Transformation
 import scalismo.statisticalmodel.MultivariateNormalDistribution
 
-case class Landmark[D <: Dim: NDSpace](id: String, point: Point[D], description: Option[String] = None, uncertainty: Option[MultivariateNormalDistribution] = None)
+case class Landmark[D <: Dim: NDSpace](id: String, point: Point[D], description: Option[String] = None, uncertainty: Option[MultivariateNormalDistribution] = None) {
+
+  /**
+   * Transforms a landmark point with the given transformation.
+   * The method transforms both the point and the uncertainty. The new uncertainty is
+   * estimated stochastically and is only an approximation to the real uncertainty
+   * (for non-rigid transformations, the uncertainty would not even be gaussian)
+   */
+  def transform(transformation: Transformation[D])(implicit random: scalismo.utils.Random): Landmark[D] = {
+    val transformedPoint = transformation(point)
+    val transformedUncertainty = uncertainty match {
+      case Some(uncertainty) => {
+
+        // in order to understand what the new uncertainty is, we simply
+        // sample from the current distribution a few points and then
+        // estimate the new uncertainty from it.
+        val transformedPoints = for (i <- 0 until 500) yield {
+          val pt = NDSpace[D].createPoint(uncertainty.sample()(random).toArray)
+          transformation(pt)
+        }
+        val newCov = MultivariateNormalDistribution.estimateFromData(transformedPoints.map(_.toBreezeVector)).cov
+        Some(MultivariateNormalDistribution(uncertainty.mean, newCov))
+      }
+      case None => None
+    }
+    this.copy(point = transformedPoint, uncertainty = transformedUncertainty)
+  }
+}
 
