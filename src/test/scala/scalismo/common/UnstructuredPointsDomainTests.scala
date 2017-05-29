4b99a1336b12ce41b377c461ecb8b2e03e7544e4
Sandro Schönborn <sandro.schoenborn@unibas.ch>
2017-05-29 14:18:29 +0200

    removed copyright year

--- src/test/scala/scalismo/common/UnstructuredPointsDomainTests.scala
+++ src/test/scala/scalismo/common/UnstructuredPointsDomainTests.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 University of Basel, Graphics and Vision Research Group
+ * Copyright University of Basel, Graphics and Vision Research Group
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
