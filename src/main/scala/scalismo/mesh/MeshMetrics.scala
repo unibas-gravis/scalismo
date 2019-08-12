8f15ccad0408f69dcd8f2ff079c66085e3daea93
dennes <dennis.madsen@unibas.ch>
2019-08-12 09:28:05 +0200

    Formatting to satisfy Travis and extended description about the type of procrustes distance that is used.

--- src/main/scala/scalismo/mesh/MeshMetrics.scala
+++ src/main/scala/scalismo/mesh/MeshMetrics.scala
@@ -16,21 +16,21 @@
 package scalismo.mesh
 
 import scalismo.common.BoxDomain
-import scalismo.geometry.{ Point, _3D }
+import scalismo.geometry.{Point, _3D}
 import scalismo.numerics.UniformSampler
 import scalismo.registration.LandmarkRegistration
 import scalismo.utils.Random
 
 /**
- * Implements utility methods for evaluating similarity of [[TriangleMesh]] instances
- *
- */
+  * Implements utility methods for evaluating similarity of [[TriangleMesh]] instances
+  *
+  */
 object MeshMetrics {
 
   /**
-   * For each point of the first mesh, this method computes the shortest distance to the surface of the
-   * second mesh and returns the average over all points
-   */
+    * For each point of the first mesh, this method computes the shortest distance to the surface of the
+    * second mesh and returns the average over all points
+    */
 
   def avgDistance(m1: TriangleMesh[_3D], m2: TriangleMesh[_3D]): Double = {
 
@@ -42,24 +42,25 @@ object MeshMetrics {
   }
 
   /**
-   * Returns the average mesh correspondence point distance after performing a rigid alignment between the two meshes.
-   * All mesh points are used for the rigid alignment, therefore both meshes must be in correspondence
-   */
+    * Partial Procrustes distance - returns the average mesh correspondence point distance after performing a rigid alignment
+    * between the two meshes. Note that no scale transformation is applied in the shape alignment.
+    * All mesh points are used for the rigid alignment, therefore both meshes must be in correspondence
+    */
   def procrustesDistance(m1: TriangleMesh[_3D], m2: TriangleMesh[_3D]): Double = {
     require(m1.pointSet.numberOfPoints == m2.pointSet.numberOfPoints)
 
     val landmarks = m1.pointSet.points.toIndexedSeq zip m2.pointSet.points.toIndexedSeq
     val t = LandmarkRegistration.rigid3DLandmarkRegistration(landmarks, Point(0, 0, 0))
     val m1w = m1.transform(t)
-    val dists = (m1w.pointSet.points.toIndexedSeq zip m2.pointSet.points.toIndexedSeq).map{
+    val dists = (m1w.pointSet.points.toIndexedSeq zip m2.pointSet.points.toIndexedSeq).map {
       case (m1wP, m2P) => (m1wP - m2P).norm
     }
     dists.sum / m1.pointSet.numberOfPoints
   }
 
   /**
-   * Returns the Hausdorff distance between the two meshes
-   */
+    * Returns the Hausdorff distance between the two meshes
+    */
   def hausdorffDistance(m1: TriangleMesh[_3D], m2: TriangleMesh[_3D]): Double = {
     def allDistsBetweenMeshes(mm1: TriangleMesh[_3D], mm2: TriangleMesh[_3D]): Iterator[Double] = {
       for (ptM1 <- mm1.pointSet.points) yield {
@@ -76,13 +77,14 @@ object MeshMetrics {
   }
 
   /**
-   * Computes a binary image for each mesh and returns the Dice Coefficient between the two images
-   */
+    * Computes a binary image for each mesh and returns the Dice Coefficient between the two images
+    */
   def diceCoefficient(m1: TriangleMesh[_3D], m2: TriangleMesh[_3D])(implicit rand: Random): Double = {
     val imgA = m1.operations.toBinaryImage
     val imgB = m2.operations.toBinaryImage
 
     def minPoint(pt1: Point[_3D], pt2: Point[_3D]) = Point(math.min(pt1(0), pt2(0)), math.min(pt1(1), pt2(1)), math.min(pt1(2), pt2(2)))
+
     def maxPoint(pt1: Point[_3D], pt2: Point[_3D]) = Point(math.max(pt1(0), pt2(0)), math.max(pt1(1), pt2(1)), math.max(pt1(2), pt2(2)))
 
     val box1 = m1.pointSet.boundingBox
