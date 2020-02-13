d31443eeb3fa860fffa7d9469f5054593df5366f
Patrick Kahr <18963412+kahrpatrick@users.noreply.github.com>
2020-02-13 14:26:02 +0100

    update BarycentricInterpolator to use MeshOperations
    make use of MeshOperations to find corresponding cell

--- src/main/scala/scalismo/common/interpolation/BarycentricInterpolator.scala
+++ src/main/scala/scalismo/common/interpolation/BarycentricInterpolator.scala
@@ -1,10 +1,10 @@
 package scalismo.common.interpolation
 
 import scalismo.common._
-import scalismo.geometry.{_3D, NDSpace, Point, Point3D}
-import scalismo.mesh.{TetrahedralCell, TetrahedralMesh, TetrahedronId}
+import scalismo.geometry.{_3D, NDSpace, Point}
+import scalismo.mesh.boundingSpheres._
+import scalismo.mesh.{TetrahedralMesh, TetrahedralMesh3DOperations}
 import scalismo.numerics.ValueInterpolator
-import scalismo.utils.Memoize
 
 trait BarycentricInterpolator[D, A] extends FieldInterpolator[D, UnstructuredPointsDomain[D], A] {
   implicit protected val valueInterpolator: ValueInterpolator[A]
@@ -33,48 +33,26 @@ object BarycentricInterpolator {
 case class BarycentricInterpolator3D[A: ValueInterpolator](mesh: TetrahedralMesh[_3D])
     extends BarycentricInterpolator[_3D, A] {
 
+  val meshOps: TetrahedralMesh3DOperations = mesh.operations
+
   override protected val valueInterpolator: ValueInterpolator[A] = ValueInterpolator[A]
-
-  // TODO: Temporary solution, replace for Milestone M2!
-  private def getTetrahedralMeshCell(p: Point[_3D]): Option[TetrahedralCell] = {
-
-    val numberOfTetrahedrons = mesh.tetrahedralization.tetrahedrons.length
-
-    def isInsideCell(tc: TetrahedralCell): Boolean = mesh.isInsideTetrahedralCell(p, tc)
-    val isInsideCellMemoized = Memoize(isInsideCell, numberOfTetrahedrons)
-
-    var cell: Option[TetrahedralCell] = None
-    var neighbourhood = Set[TetrahedronId]()
-
-    while (cell.isEmpty && neighbourhood.size != numberOfTetrahedrons) {
-      if (neighbourhood.isEmpty) {
-        // start from closest vertex point
-        val closestPoint = mesh.pointSet.findClosestPoint(p).id
-        neighbourhood = mesh.tetrahedralization.adjacentTetrahedronsForPoint(closestPoint).toSet
-      } else {
-        // increase neighbourhood
-        neighbourhood =
-          neighbourhood.union(neighbourhood.flatMap(mesh.tetrahedralization.adjacentTetrahedronsForTetrahedron))
-      }
-      val filterResult = neighbourhood.filter(tId => isInsideCellMemoized(mesh.tetrahedralization.tetrahedrons(tId.id)))
-      if (filterResult.nonEmpty) cell = Some(mesh.tetrahedralization.tetrahedrons(filterResult.head.id))
-    }
-    cell
-  }
 
   override def interpolate(df: DiscreteField[_3D, UnstructuredPointsDomain[_3D], A]): Field[_3D, A] = {
 
     def interpolateBarycentric(p: Point[_3D]): A = {
-      getTetrahedralMeshCell(p) match {
-        case Some(cell) =>
-          val vertexValues = cell.pointIds.map(df(_))
-          val barycentricCoordinates = mesh.getBarycentricCoordinates(p, cell)
-          val valueCoordinatePairs = vertexValues.zip(barycentricCoordinates)
-          ValueInterpolator[A].convexCombination(valueCoordinatePairs(0),
-                                                 valueCoordinatePairs(1),
-                                                 valueCoordinatePairs(2),
-                                                 valueCoordinatePairs(3))
-        case None => throw new Exception(s"Point $p outside of domain.")
+
+      meshOps.closestPointToVolume(p) match {
+        case ClosestPointIsVertex(_, _, pId)    => df(pId)
+        case ClosestPointOnLine(_, _, pIds, bc) => ValueInterpolator[A].blend(df(pIds._1), df(pIds._2), bc)
+        case ClosestPointInTriangleOfTetrahedron(_, _, tetId, triId, bc) =>
+          val triangle = mesh.tetrahedralization.tetrahedron(tetId).triangles(triId.id)
+          bc.interpolateProperty(df(triangle.ptId1), df(triangle.ptId2), df(triangle.ptId3))
+        case ClosestPointInTetrahedron(_, _, tId, bc) =>
+          val tetrahedron = mesh.tetrahedralization.tetrahedron(tId)
+          bc.interpolateProperty(df(tetrahedron.ptId1),
+                                 df(tetrahedron.ptId2),
+                                 df(tetrahedron.ptId3),
+                                 df(tetrahedron.ptId4))
       }
     }
     Field(RealSpace[_3D], interpolateBarycentric)
