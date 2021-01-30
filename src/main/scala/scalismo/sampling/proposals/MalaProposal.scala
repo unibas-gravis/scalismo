361ae7c211499cfef74a933925cbb57a6d5944ea
Marcel Luethi <marcel.luethi@unibas.ch>
2021-01-30 20:15:41 +0100

    updated mala proposal to new lense structure

--- src/main/scala/scalismo/sampling/proposals/MalaProposal.scala
+++ src/main/scala/scalismo/sampling/proposals/MalaProposal.scala
@@ -9,6 +9,7 @@ import scalismo.sampling.{
   TransitionProbability
 }
 import scalismo.statisticalmodel.MultivariateNormalDistribution
+import scalismo.utils.Memoize
 
 /**
  * An implmentation of the Langevin Monte carlo as a proposal. Used with the
@@ -22,28 +23,25 @@ import scalismo.statisticalmodel.MultivariateNormalDistribution
  */
 case class MalaProposal[A](evaluator: DistributionEvaluator[A] with GradientEvaluator[A],
                            tau: Double,
-                           sampleLens: SampleLens[A])(implicit rng: scalismo.utils.Random)
+                           sampleLens: SampleLens[A, DenseVector[Double]])(implicit rng: scalismo.utils.Random)
     extends ProposalGenerator[A]
     with TransitionProbability[A] {
 
-  private val normalDist = MultivariateNormalDistribution(
-    mean = DenseVector.zeros[Double](sampleLens.numberOfParameters),
-    cov = DenseMatrix.eye[Double](sampleLens.numberOfParameters)
-  )
+  private val evaluatorGradient = Memoize(evaluator.gradient, 10)
 
   override def propose(sample: A): A = {
 
-    val gradient = sampleLens.getAsVector(evaluator.gradient(sample))
-    val normalSample = normalDist.sample() * Math.sqrt(2.0 * tau)
-    val newParameters = sampleLens.getAsVector(sample) + gradient * tau + normalSample
-    sampleLens.setFromVector(sample, newParameters, Some(s"GradientUpdatePropsosal ($tau"))
+    val gradient = sampleLens.get(evaluatorGradient(sample))
+    val perturbation = DenseVector.rand(gradient.length, rng.breezeRandBasis.gaussian) * Math.sqrt(2.0 * tau)
+    val newParameters = sampleLens.get(sample) + gradient * tau + perturbation
+    sampleLens.replace(sample, newParameters, Some(s"MalaProposal (tau=$tau)"))
   }
 
   override def logTransitionProbability(from: A, to: A): Double = {
-    val g = sampleLens.getAsVector(evaluator.gradient(from))
+    val g = sampleLens.get(evaluatorGradient(from))
     Math.exp(
       breeze.linalg
-        .norm(sampleLens.getAsVector(to) - sampleLens.getAsVector(from) - g * tau, 2) * (-1.0 / (4 * tau))
+        .norm(sampleLens.get(to) - sampleLens.get(from) - g * tau, 2) * (-1.0 / (4 * tau))
     )
   }
 }
