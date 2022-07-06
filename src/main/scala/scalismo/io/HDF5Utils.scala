0106244acf997038251f14492d9a5e210aa5f65b
Ghazi Bouabene <ghazi.bouabene@shapemeans.com>
2022-07-06 14:16:09 +0200

    fixed attribute casting when reading old (Statismo) .h5 files

--- src/main/scala/scalismo/io/HDF5Utils.scala
+++ src/main/scala/scalismo/io/HDF5Utils.scala
@@ -52,24 +52,28 @@ class HDF5Reader(h5file: HdfFile) extends Closeable {
 
   def readStringAttribute(path: String, attrName: String): Try[String] = {
     Try {
-      h5file
+      val attribute = h5file
         .getByPath(sanitizePath(path))
         .getAttribute(attrName)
         .getData()
-        .asInstanceOf[Array[String]]
-        .head
+
+      if(attribute.isInstanceOf[String])
+        attribute.asInstanceOf[String]
+      else attribute.asInstanceOf[Array[String]].head
     }
   }
 
   def readIntAttribute(path: String, attrName: String): Try[Int] = {
 
     Try {
-      h5file
+      val attribute = h5file
         .getByPath(sanitizePath(path))
         .getAttribute(attrName)
         .getData()
-        .asInstanceOf[Array[Int]]
-        .head
+
+      if(attribute.isInstanceOf[Int])
+        attribute.asInstanceOf[Int]
+      else attribute.asInstanceOf[Array[Int]].head
     }
 
   }
