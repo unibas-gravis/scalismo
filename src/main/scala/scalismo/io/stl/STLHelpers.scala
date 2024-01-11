a118f333eb6e5d88dd7864b0bb67e8d3a7b912e6
dennis@dentexion.com <dennis.madsen@unibas.ch>
2024-01-11 23:11:01 +0100

    feat: simple binary stl reader added

--- src/main/scala/scalismo/io/stl/STLHelpers.scala
+++ src/main/scala/scalismo/io/stl/STLHelpers.scala
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 University of Basel, Graphics and Vision Research Group
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package scalismo.io.stl
 
 import scalismo.common.PointId
@@ -30,17 +15,16 @@ case class STLTriangle(n: EuclideanVector3D, p1: Point3D, p2: Point3D, p3: Point
 package object STLHelpers {
   def STLTrianglesToTriangleMesh(stlTriangles: Seq[STLTriangle]): TriangleMesh3D = {
     val pointsMap = mutable.HashMap.empty[Point3D, Int]
-    val pointsArray = new Array[Point3D](stlTriangles.length * 3)
+    var pointsArray = new Array[Point3D](stlTriangles.length * 3)
     var pointCounter = 0
 
     def getOrCreatePointId(point: Point3D): Int =
       pointsMap.getOrElseUpdate(point, {
-                                  val id = pointCounter
-                                  pointsArray(id) = point
-                                  pointCounter += 1
-                                  id
-                                }
-      )
+        val id = pointCounter
+        pointsArray(id) = point
+        pointCounter += 1
+        id
+      })
 
     val triangles = stlTriangles.map { triangle =>
       val ptId1 = getOrCreatePointId(triangle.p1)
