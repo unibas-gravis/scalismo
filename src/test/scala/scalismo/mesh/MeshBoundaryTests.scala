c4869cfc6f99e9f4b5895c9eed069c606b295a5a
dennis.madsen <dennis.madsen@unibas.ch>
2024-01-16 10:59:06 +0100

    chore: moved vtk dependent tests from mesh tests to scalismo vtk

--- src/test/scala/scalismo/mesh/MeshBoundaryTests.scala
+++ src/test/scala/scalismo/mesh/MeshBoundaryTests.scala
@@ -15,14 +15,13 @@
  */
 package scalismo.mesh
 
-import java.io.File
-import java.net.URLDecoder
-
 import scalismo.ScalismoTestSuite
 import scalismo.common.PointId
 import scalismo.geometry.Point
 import scalismo.io.MeshIO
 
+import java.io.File
+import java.net.URLDecoder
 import scala.language.implicitConversions
 
 class MeshBoundaryTests extends ScalismoTestSuite {
@@ -64,8 +63,10 @@ class MeshBoundaryTests extends ScalismoTestSuite {
       boundaryPointIds.length shouldBe 918
       boundaryTriangleIds.length shouldBe 782
 
-      val allBoundaryPointIds = meshNoTriangles.pointSet.pointIds.toSeq.filter(id => meshNoTriangles.operations.pointIsOnBoundary(id))
-      val allBoundaryTriangleIds = meshNoTriangles.triangulation.triangleIds.filter(id => meshNoTriangles.operations.triangleIsOnBoundary(id))
+      val allBoundaryPointIds =
+        meshNoTriangles.pointSet.pointIds.toSeq.filter(id => meshNoTriangles.operations.pointIsOnBoundary(id))
+      val allBoundaryTriangleIds =
+        meshNoTriangles.triangulation.triangleIds.filter(id => meshNoTriangles.operations.triangleIsOnBoundary(id))
       allBoundaryPointIds.length shouldBe 0
       allBoundaryTriangleIds.length shouldBe 0
     }
@@ -315,49 +316,49 @@ class MeshBoundaryTests extends ScalismoTestSuite {
     }
   }
 
-  describe("a tetrahedral mesh boundary") {
-    it("should return the correct labels for the tetrahedrons in the test mesh") {
-
-      val path = getClass.getResource("/tetraMesh.vtk").getPath
-      val tmesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
-
-      val tetsOnBoundary = IndexedSeq(0, 1, 3, 4, 8, 9, 11, 12, 16, 17, 19, 20, 24, 25, 27, 28, 32, 33, 35, 36, 40, 41,
-                                      43, 47, 48, 49, 51, 52, 56, 57, 59, 60, 64, 65, 67, 68, 72, 73, 75, 76, 80, 81,
-                                      83, 84, 88, 89, 91, 95, 96, 97, 99, 100, 104, 105, 107, 111, 112, 113, 115, 116,
-                                      120, 121, 123, 124, 128, 129, 131, 132, 136, 137, 139, 140, 144, 145, 147, 148,
-                                      152, 153, 155, 156).map(i => TetrahedronId(i))
-      tetsOnBoundary.map { tet =>
-        require(tmesh.operations.tetrahedronIsOnBoundary(tet))
-      }
-
-      val tetsInside = IndexedSeq(2, 5, 6, 7, 10, 13, 14, 15, 18, 21, 22, 23, 26, 29, 30, 31, 34, 37, 38, 39, 42, 44,
-                                  45, 46, 50, 53, 54, 55, 58, 61, 62, 63, 66, 69, 70, 71, 74, 77, 78, 79, 82, 85, 86,
-                                  87, 90, 92, 93, 94, 98, 101, 102, 103, 106, 108, 109, 110, 114, 117, 118, 119, 122,
-                                  125, 126, 127, 130, 133, 134, 135, 138, 141, 142, 143, 146, 149, 150, 151, 154, 157,
-                                  158, 159).map(i => TetrahedronId(i))
-      tetsInside.map { tet =>
-        require(!tmesh.operations.tetrahedronIsOnBoundary(tet))
-      }
-    }
-
-    it("should return the correct labels for the points in the test mesh") {
-
-      val path = getClass.getResource("/tetraMesh.vtk").getPath
-      val tmesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
-
-      val innerPoints = IndexedSeq(12, 14, 15, 18, 20, 21, 24, 26, 33, 35, 36, 45, 51).map(i => PointId(i))
-      innerPoints.map { pid =>
-        require(!tmesh.operations.pointIsOnBoundary(pid))
-      }
-
-      val surfacePoints =
-        IndexedSeq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 16, 17, 19, 22, 23, 25, 27, 28, 29, 30, 31, 32, 34, 37, 38,
-                   39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 52, 53, 54).map(i => PointId(i))
-      surfacePoints.map { pid =>
-        require(tmesh.operations.pointIsOnBoundary(pid))
-      }
-    }
-
-    it("should return the correct labels") {}
-  }
+//  describe("a tetrahedral mesh boundary") {
+//    it("should return the correct labels for the tetrahedrons in the test mesh") {
+//
+//      val path = getClass.getResource("/tetraMesh.vtk").getPath
+//      val tmesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
+//
+//      val tetsOnBoundary = IndexedSeq(0, 1, 3, 4, 8, 9, 11, 12, 16, 17, 19, 20, 24, 25, 27, 28, 32, 33, 35, 36, 40, 41,
+//                                      43, 47, 48, 49, 51, 52, 56, 57, 59, 60, 64, 65, 67, 68, 72, 73, 75, 76, 80, 81,
+//                                      83, 84, 88, 89, 91, 95, 96, 97, 99, 100, 104, 105, 107, 111, 112, 113, 115, 116,
+//                                      120, 121, 123, 124, 128, 129, 131, 132, 136, 137, 139, 140, 144, 145, 147, 148,
+//                                      152, 153, 155, 156).map(i => TetrahedronId(i))
+//      tetsOnBoundary.map { tet =>
+//        require(tmesh.operations.tetrahedronIsOnBoundary(tet))
+//      }
+//
+//      val tetsInside = IndexedSeq(2, 5, 6, 7, 10, 13, 14, 15, 18, 21, 22, 23, 26, 29, 30, 31, 34, 37, 38, 39, 42, 44,
+//                                  45, 46, 50, 53, 54, 55, 58, 61, 62, 63, 66, 69, 70, 71, 74, 77, 78, 79, 82, 85, 86,
+//                                  87, 90, 92, 93, 94, 98, 101, 102, 103, 106, 108, 109, 110, 114, 117, 118, 119, 122,
+//                                  125, 126, 127, 130, 133, 134, 135, 138, 141, 142, 143, 146, 149, 150, 151, 154, 157,
+//                                  158, 159).map(i => TetrahedronId(i))
+//      tetsInside.map { tet =>
+//        require(!tmesh.operations.tetrahedronIsOnBoundary(tet))
+//      }
+//    }
+//
+//    it("should return the correct labels for the points in the test mesh") {
+//
+//      val path = getClass.getResource("/tetraMesh.vtk").getPath
+//      val tmesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
+//
+//      val innerPoints = IndexedSeq(12, 14, 15, 18, 20, 21, 24, 26, 33, 35, 36, 45, 51).map(i => PointId(i))
+//      innerPoints.map { pid =>
+//        require(!tmesh.operations.pointIsOnBoundary(pid))
+//      }
+//
+//      val surfacePoints =
+//        IndexedSeq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 16, 17, 19, 22, 23, 25, 27, 28, 29, 30, 31, 32, 34, 37, 38,
+//                   39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 52, 53, 54).map(i => PointId(i))
+//      surfacePoints.map { pid =>
+//        require(tmesh.operations.pointIsOnBoundary(pid))
+//      }
+//    }
+//
+//    it("should return the correct labels") {}
+//  }
 }
--- src/test/scala/scalismo/mesh/MeshMetricsTests.scala
+++ src/test/scala/scalismo/mesh/MeshMetricsTests.scala
@@ -15,14 +15,14 @@
  */
 package scalismo.mesh
 
-import java.io.File
-import java.net.URLDecoder
-
 import scalismo.ScalismoTestSuite
 import scalismo.common.PointId
-import scalismo.geometry.{_3D, EuclideanVector, Point}
+import scalismo.geometry.{EuclideanVector, Point, _3D}
 import scalismo.io.MeshIO
 import scalismo.utils.Random
+
+import java.io.File
+import java.net.URLDecoder
 
 class MeshMetricsTests extends ScalismoTestSuite {
 
@@ -101,26 +101,26 @@ class MeshMetricsTests extends ScalismoTestSuite {
 
 }
 
-class tetrahedralMeshMetricsTests extends ScalismoTestSuite {
-
-  implicit val rng: Random = Random(42L)
-  final val epsilon = 1e-8
-
-  val path = getClass.getResource("/tetraMesh.vtk").getPath
-  val mesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
-  val translationLength = 1.0
-  val translatedMesh = mesh.transform((pt: Point[_3D]) => pt + EuclideanVector(translationLength, 0.0, 0.0))
-
-  describe("The ProcrustesDistanceMetric") {
-
-    it("yields 0 between the same tetrahedral mesh") {
-      MeshMetrics.procrustesDistance(mesh, mesh) should be(0.0 +- epsilon)
-    }
-
-    it("should be 0 for a translated mesh") {
-      MeshMetrics.procrustesDistance(mesh, translatedMesh) should be(0.0 +- epsilon)
-    }
-
-  }
-
-}
+//class tetrahedralMeshMetricsTests extends ScalismoTestSuite {
+//
+//  implicit val rng: Random = Random(42L)
+//  final val epsilon = 1e-8
+//
+//  val path = getClass.getResource("/tetraMesh.vtk").getPath
+//  val mesh = MeshIO.readTetrahedralMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
+//  val translationLength = 1.0
+//  val translatedMesh = mesh.transform((pt: Point[_3D]) => pt + EuclideanVector(translationLength, 0.0, 0.0))
+//
+//  describe("The ProcrustesDistanceMetric") {
+//
+//    it("yields 0 between the same tetrahedral mesh") {
+//      MeshMetrics.procrustesDistance(mesh, mesh) should be(0.0 +- epsilon)
+//    }
+//
+//    it("should be 0 for a translated mesh") {
+//      MeshMetrics.procrustesDistance(mesh, translatedMesh) should be(0.0 +- epsilon)
+//    }
+//
+//  }
+//
+//}
