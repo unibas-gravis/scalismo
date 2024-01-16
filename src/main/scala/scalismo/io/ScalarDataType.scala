2f4d34f2ee3bee956eea8b3a91048972aaea32f5
dennis.madsen <dennis.madsen@unibas.ch>
2024-01-16 10:10:53 +0100

    chore: removed vtk from scalardatatype

--- src/main/scala/scalismo/io/ScalarDataType.scala
+++ src/main/scala/scalismo/io/ScalarDataType.scala
@@ -20,11 +20,8 @@ import niftijio.NiftiHeader
 import scalismo.common.Scalar
 import scalismo.utils.VtkHelpers
 import spire.math.{UByte, UInt, UShort}
-import vtk.{vtkObjectBase, vtkStructuredPointsReader}
 
-import java.io.{File, IOException}
 import scala.reflect.ClassTag
-import scala.util.{Failure, Try}
 
 /**
  * An enumeration comprising all the data types that we can read and write, in VTK and Nifti formats.
@@ -101,40 +98,5 @@ private[scalismo] object ScalarDataType extends Enumeration {
     values
       .find(v => v.niftiId == niftiId)
       .getOrElse(throw new IllegalArgumentException(s"Unsupported Nifti ID $niftiId"))
-  }
-
-  /**
-   * Return the ScalarType value corresponding to the data present in a given file. Only .vtk, .nii and .nia files are
-   * supported.
-   * @param file
-   *   the file to check
-   * @return
-   *   the scalar type present in the given file, wrapped in a [[scala.util.Success]], or a [[scala.util.Failure]]
-   *   explaining the error.
-   */
-  def ofFile(file: File): Try[ScalarDataType.Value] = {
-    val fn = file.getName
-    if (fn.endsWith(".nii") || fn.endsWith(".nia")) {
-      FastReadOnlyNiftiVolume.getScalarType(file)
-    } else if (fn.endsWith(".vtk")) Try {
-      val reader = new vtkStructuredPointsReader
-      reader.SetFileName(file.getAbsolutePath)
-      reader.Update()
-      val errCode = reader.GetErrorCode()
-      if (errCode != 0) {
-        reader.Delete()
-        throw new IOException(
-          s"Failed to read vtk file ${file.getAbsolutePath}. (error code from vtkReader = $errCode)"
-        )
-      }
-      val st = reader.GetOutput().GetScalarType()
-      reader.Delete()
-      // prevent memory leaks
-      vtkObjectBase.JAVA_OBJECT_MANAGER.gc(false)
-      ScalarDataType.fromVtkId(st)
-    }
-    else {
-      Failure(new Exception(s"File $file: unsupported file extension"))
-    }
   }
 }
