fe29e02edd40ab144803d7160041346c96654c7f
dennis.madsen <dennis.madsen@unibas.ch>
2024-01-16 10:53:03 +0100

    chore: moved vtk dependent test from meshiotests to scalismo-vtk

--- src/test/scala/scalismo/io/MeshIOTests.scala
+++ src/test/scala/scalismo/io/MeshIOTests.scala
@@ -15,19 +15,19 @@
  */
 package scalismo.io
 
-import java.io.File
-import java.net.URLDecoder
 import scalismo.ScalismoTestSuite
 import scalismo.common.DiscreteField.{ScalarMeshField, ScalarVolumeMeshField}
-import scalismo.common.{DiscreteField, PointId, Scalar, ScalarArray, ScalarMeshField, UnstructuredPoints}
-import scalismo.geometry.{_3D, Point}
+import scalismo.common.*
+import scalismo.geometry.{Point, _3D}
 import scalismo.io.MeshIOTests.{createRandomScalarVolumeMeshField, createRandomTetrahedralMesh}
-import scalismo.mesh._
+import scalismo.mesh.*
 import scalismo.utils.Random
 
+import java.io.File
+import java.net.URLDecoder
+import scala.language.implicitConversions
 import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
-import scala.language.implicitConversions
 
 class MeshIOTests extends ScalismoTestSuite {
 
@@ -48,25 +48,25 @@ class MeshIOTests extends ScalismoTestSuite {
           mesh should equal(origMesh)
         }
       }
-      testWriteRead(".vtk")
       testWriteRead(".stl")
+      testWriteRead(".ply")
     }
 
-    it("yields the original polyline when reading  and writing a polyLine in 2D") {
-      val path = getClass.getResource("/linemesh.vtk").getPath
-      val origMesh = MeshIO.readLineMesh2D(new File(URLDecoder.decode(path, "UTF-8"))).get
-
-      val tmpFile = File.createTempFile("mesh", ".vtk")
-      val writeStatus = MeshIO.writeLineMesh(origMesh, tmpFile)
-      writeStatus.isSuccess should be(true)
-
-      val meshTry = MeshIO.readLineMesh2D(tmpFile)
-      meshTry.isSuccess should be(true)
-      meshTry.map { mesh =>
-        mesh should equal(origMesh)
-      }
-
-    }
+//    it("yields the original polyline when reading  and writing a polyLine in 2D") {
+//      val path = getClass.getResource("/linemesh.vtk").getPath
+//      val origMesh = MeshIO.readLineMesh2D(new File(URLDecoder.decode(path, "UTF-8"))).get
+//
+//      val tmpFile = File.createTempFile("mesh", ".vtk")
+//      val writeStatus = MeshIO.writeLineMesh(origMesh, tmpFile)
+//      writeStatus.isSuccess should be(true)
+//
+//      val meshTry = MeshIO.readLineMesh2D(tmpFile)
+//      meshTry.isSuccess should be(true)
+//      meshTry.map { mesh =>
+//        mesh should equal(origMesh)
+//      }
+//
+//    }
 
     it("yields the original mesh when reading and writing a shape only ply") {
       val path = getClass.getResource("/mean_shapeOnly.ply").getPath
@@ -100,161 +100,161 @@ class MeshIOTests extends ScalismoTestSuite {
       assert(shape.isFailure)
     }
 
-    it("correctly writes and reads a tetrahedral mesh to a vtk file ") {
-      val temporaryFile = File.createTempFile("tetrahedralMesh", ".vtk")
-      val originalMesh = createRandomTetrahedralMesh()
-      MeshIO.writeTetrahedralMesh(originalMesh, temporaryFile)
-      val loadingMesh = MeshIO.readTetrahedralMesh(temporaryFile)
-      assert(loadingMesh.isSuccess)
-      loadingMesh.map { loadedMesh =>
-        assert(loadedMesh.pointSet == originalMesh.pointSet)
-        assert(loadedMesh.tetrahedralization == originalMesh.tetrahedralization)
-      }
-    }
+//    it("correctly writes and reads a tetrahedral mesh to a vtk file ") {
+//      val temporaryFile = File.createTempFile("tetrahedralMesh", ".vtk")
+//      val originalMesh = createRandomTetrahedralMesh()
+//      MeshIO.writeTetrahedralMesh(originalMesh, temporaryFile)
+//      val loadingMesh = MeshIO.readTetrahedralMesh(temporaryFile)
+//      assert(loadingMesh.isSuccess)
+//      loadingMesh.map { loadedMesh =>
+//        assert(loadedMesh.pointSet == originalMesh.pointSet)
+//        assert(loadedMesh.tetrahedralization == originalMesh.tetrahedralization)
+//      }
+//    }
 
-    it("correctly writes and reads a tetrahedral mesh to a vtu file ") {
-      val temporaryFile = File.createTempFile("tetrahedralMesh", ".vtu")
-      val originalMesh = createRandomTetrahedralMesh()
-      MeshIO.writeTetrahedralMesh(originalMesh, temporaryFile)
-      val loadingMesh = MeshIO.readTetrahedralMesh(temporaryFile)
-      assert(loadingMesh.isSuccess)
-      loadingMesh.map { loadedMesh =>
-        assert(loadedMesh.pointSet == loadedMesh.pointSet)
-        assert(loadedMesh.tetrahedralization == originalMesh.tetrahedralization)
-      }
-    }
+//    it("correctly writes and reads a tetrahedral mesh to a vtu file ") {
+//      val temporaryFile = File.createTempFile("tetrahedralMesh", ".vtu")
+//      val originalMesh = createRandomTetrahedralMesh()
+//      MeshIO.writeTetrahedralMesh(originalMesh, temporaryFile)
+//      val loadingMesh = MeshIO.readTetrahedralMesh(temporaryFile)
+//      assert(loadingMesh.isSuccess)
+//      loadingMesh.map { loadedMesh =>
+//        assert(loadedMesh.pointSet == loadedMesh.pointSet)
+//        assert(loadedMesh.tetrahedralization == originalMesh.tetrahedralization)
+//      }
+//    }
 
-    it("correctly writes and reads a scalar volume mesh field to a vtk file ") {
-      val temporaryFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
-      val originalMesh = createRandomScalarVolumeMeshField()
-      MeshIO.writeScalarVolumeMeshField(originalMesh, temporaryFile)
-      val loadingMesh = MeshIO.readScalarVolumeMeshField[Int](temporaryFile)
-      assert(loadingMesh.isSuccess)
-      loadingMesh.map { loadedMesh =>
-        assert(loadedMesh.domain.pointSet == originalMesh.domain.pointSet)
-        assert(loadedMesh.domain.tetrahedralization == originalMesh.domain.tetrahedralization)
-        assert(loadedMesh.data == originalMesh.data)
-      }
-    }
+//    it("correctly writes and reads a scalar volume mesh field to a vtk file ") {
+//      val temporaryFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
+//      val originalMesh = createRandomScalarVolumeMeshField()
+//      MeshIO.writeScalarVolumeMeshField(originalMesh, temporaryFile)
+//      val loadingMesh = MeshIO.readScalarVolumeMeshField[Int](temporaryFile)
+//      assert(loadingMesh.isSuccess)
+//      loadingMesh.map { loadedMesh =>
+//        assert(loadedMesh.domain.pointSet == originalMesh.domain.pointSet)
+//        assert(loadedMesh.domain.tetrahedralization == originalMesh.domain.tetrahedralization)
+//        assert(loadedMesh.data == originalMesh.data)
+//      }
+//    }
 
-    it("correctly writes and reads a scalar volume mesh field to a vtu file ") {
-      val temporaryFile = File.createTempFile("scalarVolumeMeshField", ".vtu")
-      val originalMesh = createRandomScalarVolumeMeshField()
-      MeshIO.writeScalarVolumeMeshField(originalMesh, temporaryFile)
-      val loadingMesh = MeshIO.readScalarVolumeMeshField[Int](temporaryFile)
-      assert(loadingMesh.isSuccess)
-      loadingMesh.map { loadedMesh =>
-        assert(loadedMesh.domain.pointSet == originalMesh.domain.pointSet)
-        assert(loadedMesh.domain.tetrahedralization == originalMesh.domain.tetrahedralization)
-        assert(loadedMesh.data == originalMesh.data)
-      }
-    }
-
-  }
-
-  describe("ScalarMeshField IO") {
-
-    object Fixture {
-      val path: String = getClass.getResource("/facemesh.stl").getPath
-      val mesh: TriangleMesh[_3D] = MeshIO.readMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
-      val meshData: ScalarMeshField[Int] = ScalarMeshField(mesh, ScalarArray(mesh.pointSet.pointIds.map(_.id).toArray))
-    }
-
-    def sameWriteRead[S: Scalar: ClassTag](): Try[ScalarMeshField[S]] = {
-      val f = Fixture
-      val tmpFile = File.createTempFile("scalarMesh", ".vtk")
-
-      val writeTry = MeshIO.writeScalarMeshField(f.meshData, tmpFile)
-      assert(writeTry.isSuccess)
-
-      MeshIO.readScalarMeshField[S](tmpFile)
-    }
-
-    it("can write and correctly read a ScalarMeshField") {
-
-      val f = Fixture
-      val readTry = sameWriteRead[Int]()
-      assert(readTry.isSuccess)
-
-      readTry.get.data == ScalarArray(f.mesh.pointSet.pointIds.map(_.id).toArray)
-    }
-
-    it("fails when reading with the wrong Scalar type") {
-      val readTry = sameWriteRead[Float]()
-      assert(readTry.isFailure)
-    }
-
-    it("can read and cast a ScalarMeshField") {
-      val f = Fixture
-      val tmpFile = File.createTempFile("scalarMesh", ".vtk")
-      tmpFile.deleteOnExit()
-      MeshIO.writeScalarMeshField(f.meshData, tmpFile)
-
-      MeshIO.readScalarMeshFieldAsType[Float](tmpFile) match {
-        case Success(meshData) =>
-          for ((origDatum, newDatum) <- f.meshData.data.zip(meshData.data)) {
-            origDatum.toFloat should equal(newDatum)
-          }
-        case Failure(t) =>
-          println(t.getMessage)
-          t.printStackTrace()
-          throw t
-      }
-
-    }
+//    it("correctly writes and reads a scalar volume mesh field to a vtu file ") {
+//      val temporaryFile = File.createTempFile("scalarVolumeMeshField", ".vtu")
+//      val originalMesh = createRandomScalarVolumeMeshField()
+//      MeshIO.writeScalarVolumeMeshField(originalMesh, temporaryFile)
+//      val loadingMesh = MeshIO.readScalarVolumeMeshField[Int](temporaryFile)
+//      assert(loadingMesh.isSuccess)
+//      loadingMesh.map { loadedMesh =>
+//        assert(loadedMesh.domain.pointSet == originalMesh.domain.pointSet)
+//        assert(loadedMesh.domain.tetrahedralization == originalMesh.domain.tetrahedralization)
+//        assert(loadedMesh.data == originalMesh.data)
+//      }
+//    }
 
   }
 
-  describe("ScalarVolumeMeshField IO") {
+//  describe("ScalarMeshField IO") {
+//
+//    object Fixture {
+//      val path: String = getClass.getResource("/facemesh.stl").getPath
+//      val mesh: TriangleMesh[_3D] = MeshIO.readMesh(new File(URLDecoder.decode(path, "UTF-8"))).get
+//      val meshData: ScalarMeshField[Int] = ScalarMeshField(mesh, ScalarArray(mesh.pointSet.pointIds.map(_.id).toArray))
+//    }
+//
+//    def sameWriteRead[S: Scalar: ClassTag](): Try[ScalarMeshField[S]] = {
+//      val f = Fixture
+//      val tmpFile = File.createTempFile("scalarMesh", ".vtk")
+//
+//      val writeTry = MeshIO.writeScalarMeshField(f.meshData, tmpFile)
+//      assert(writeTry.isSuccess)
+//
+//      MeshIO.readScalarMeshField[S](tmpFile)
+//    }
+//
+//    it("can write and correctly read a ScalarMeshField") {
+//
+//      val f = Fixture
+//      val readTry = sameWriteRead[Int]()
+//      assert(readTry.isSuccess)
+//
+//      readTry.get.data == ScalarArray(f.mesh.pointSet.pointIds.map(_.id).toArray)
+//    }
+//
+//    it("fails when reading with the wrong Scalar type") {
+//      val readTry = sameWriteRead[Float]()
+//      assert(readTry.isFailure)
+//    }
+//
+//    it("can read and cast a ScalarMeshField") {
+//      val f = Fixture
+//      val tmpFile = File.createTempFile("scalarMesh", ".vtk")
+//      tmpFile.deleteOnExit()
+//      MeshIO.writeScalarMeshField(f.meshData, tmpFile)
+//
+//      MeshIO.readScalarMeshFieldAsType[Float](tmpFile) match {
+//        case Success(meshData) =>
+//          for ((origDatum, newDatum) <- f.meshData.data.zip(meshData.data)) {
+//            origDatum.toFloat should equal(newDatum)
+//          }
+//        case Failure(t) =>
+//          println(t.getMessage)
+//          t.printStackTrace()
+//          throw t
+//      }
+//
+//    }
+//
+//  }
 
-    object Fixture {
-      val meshData: ScalarVolumeMeshField[Int] = createRandomScalarVolumeMeshField()
-      val mesh = meshData.domain
-    }
-
-    def sameWriteRead[S: Scalar: ClassTag](): Try[ScalarVolumeMeshField[S]] = {
-      val f = Fixture
-      val tmpFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
-
-      val writeTry = MeshIO.writeScalarVolumeMeshField(f.meshData, tmpFile)
-      assert(writeTry.isSuccess)
-
-      MeshIO.readScalarVolumeMeshField[S](tmpFile)
-    }
-
-    it("can write and correctly read a ScalarVolumeMeshField") {
-      val f = Fixture
-      val readTry = sameWriteRead[Int]()
-      assert(readTry.isSuccess)
-
-      readTry.get.data == ScalarArray(f.mesh.pointSet.pointIds.map(_.id).toArray)
-    }
-
-    it("fails when reading with the wrong Scalar type") {
-      val readTry = sameWriteRead[Float]()
-      assert(readTry.isFailure)
-    }
-
-    it("can read and cast a ScalarVolumeMeshField") {
-      val f = Fixture
-      val tmpFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
-      tmpFile.deleteOnExit()
-      MeshIO.writeScalarVolumeMeshField(f.meshData, tmpFile)
-
-      MeshIO.readScalarVolumeMeshFieldAsType[Float](tmpFile) match {
-        case Success(meshData) =>
-          for ((origDatum, newDatum) <- f.meshData.data.zip(meshData.data)) {
-            origDatum.toInt should equal(newDatum)
-          }
-        case Failure(t) =>
-          println(t.getMessage)
-          t.printStackTrace()
-          throw t
-      }
-
-    }
-
-  }
+//  describe("ScalarVolumeMeshField IO") {
+//
+//    object Fixture {
+//      val meshData: ScalarVolumeMeshField[Int] = createRandomScalarVolumeMeshField()
+//      val mesh = meshData.domain
+//    }
+//
+//    def sameWriteRead[S: Scalar: ClassTag](): Try[ScalarVolumeMeshField[S]] = {
+//      val f = Fixture
+//      val tmpFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
+//
+//      val writeTry = MeshIO.writeScalarVolumeMeshField(f.meshData, tmpFile)
+//      assert(writeTry.isSuccess)
+//
+//      MeshIO.readScalarVolumeMeshField[S](tmpFile)
+//    }
+//
+//    it("can write and correctly read a ScalarVolumeMeshField") {
+//      val f = Fixture
+//      val readTry = sameWriteRead[Int]()
+//      assert(readTry.isSuccess)
+//
+//      readTry.get.data == ScalarArray(f.mesh.pointSet.pointIds.map(_.id).toArray)
+//    }
+//
+//    it("fails when reading with the wrong Scalar type") {
+//      val readTry = sameWriteRead[Float]()
+//      assert(readTry.isFailure)
+//    }
+//
+//    it("can read and cast a ScalarVolumeMeshField") {
+//      val f = Fixture
+//      val tmpFile = File.createTempFile("scalarVolumeMeshField", ".vtk")
+//      tmpFile.deleteOnExit()
+//      MeshIO.writeScalarVolumeMeshField(f.meshData, tmpFile)
+//
+//      MeshIO.readScalarVolumeMeshFieldAsType[Float](tmpFile) match {
+//        case Success(meshData) =>
+//          for ((origDatum, newDatum) <- f.meshData.data.zip(meshData.data)) {
+//            origDatum.toInt should equal(newDatum)
+//          }
+//        case Failure(t) =>
+//          println(t.getMessage)
+//          t.printStackTrace()
+//          throw t
+//      }
+//
+//    }
+//
+//  }
 
 }
 
