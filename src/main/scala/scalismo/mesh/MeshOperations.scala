b5d80eaa8bedb6371b3febfa89fa4db767f3486a
dennis.madsen <dennis.madsen@unibas.ch>
2024-01-16 10:07:45 +0100

    chore: removed vtk from meshoperations

--- src/main/scala/scalismo/mesh/MeshOperations.scala
+++ src/main/scala/scalismo/mesh/MeshOperations.scala
@@ -34,7 +34,6 @@ import scalismo.mesh.boundingSpheres.{
   TriangulatedSurfaceIntersectionIndex,
   VolumeSpatialIndex
 }
-import scalismo.utils.MeshConversion
 
 import scala.collection.parallel.immutable.ParVector
 
