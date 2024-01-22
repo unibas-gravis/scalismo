8da2ee93bad89e34f89f07d06be08e57f0d66d9e
Dennis Madsen <dennis@dentexion.com>
2024-01-22 13:22:20 +0100

    Feat/native ply io (#418)
    * feat: native ply writer implementation (20 times faster than VTK)
    * feat: native ply binary reader for triangleMesh
    * feat: added writing support of VertexColorMesh3D
    * feat: added support for ascii format

---
+++ src/main/scala/scalismo/io/ply/PLYHeader.scala
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2015 University of Basel, Graphics and Vision Research Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scalismo.io.ply
+
+import java.io.IOException
+import scala.collection.mutable.ArrayBuffer
+import scala.util.{Failure, Success, Try}
+
+object PLYHeader {
+
+  def createHeader(numVertices: Int, numFaces: Int, vertexColors: Boolean): String = {
+    val header = new StringBuilder
+    header.append("ply\nformat binary_little_endian 1.0\ncomment Scalismo generated PLY File\n")
+    header.append(f"element vertex $numVertices\nproperty float x\nproperty float y\nproperty float z\n")
+    if (vertexColors) {
+      header.append("property uchar red\nproperty uchar green\nproperty uchar blue\nproperty uchar alpha\n")
+    }
+    header.append(f"element face $numFaces\nproperty list uchar int vertex_indices\nend_header\n")
+    header.toString()
+  }
+
+  def parseHeader(header: Array[String]): HeaderInfo = {
+    val fileComments: ArrayBuffer[String] = ArrayBuffer.empty[String]
+    var fileFormat: Option[PLYFormat] = None
+    val fileElements: ArrayBuffer[PLYElement] = ArrayBuffer.empty[PLYElement]
+
+    var cnt = 0
+    while (cnt < header.length) {
+      val line = header(cnt)
+      val lineSplit = line.split(" ")
+      val name = lineSplit.head
+      if (name == "comment" || name == "obj_info") {
+        fileComments += lineSplit.drop(1).mkString(" ")
+      } else if (lineSplit.length > 2) {
+        if (name == "format") {
+          lineSplit(1) match {
+            case PLY_FORMAT_ASCII =>
+              fileFormat = Option(PLYFormat(PLYTypeFormat.ASCII, lineSplit(2)))
+            case PLY_FORMAT_BIG =>
+              fileFormat = Option(PLYFormat(PLYTypeFormat.BINARY_BIG_ENDIAN, lineSplit(2)))
+            case PLY_FORMAT_LITTLE =>
+              fileFormat = Option(PLYFormat(PLYTypeFormat.BINARY_LITTLE_ENDIAN, lineSplit(2)))
+            case _ => Failure(new IOException(s"Unsupported PLY format"))
+          }
+        }
+        if (name == "element") {
+          val elementFormat = lineSplit(1) match {
+            case PLY_ELEMENT_VERTEX =>
+              Success(PLYElementFormat.VERTEX)
+            case PLY_ELEMENT_FACE =>
+              Success(PLYElementFormat.FACE)
+            case _ => Failure(new IOException(s"Unsupported property format"))
+          }
+          val elementCount = lineSplit(2).toInt
+          val elementProperties: ArrayBuffer[PLYProperty] = ArrayBuffer.empty[PLYProperty]
+          while (cnt < header.length - 1 && (header(cnt + 1).split(" ").head) == "property") {
+            val nextLine = header(cnt + 1).split(" ")
+            if (nextLine(1) == "list") {
+              elementProperties += PLYProperty(nextLine(2), nextLine(4), Option(nextLine(3)))
+            } else {
+              elementProperties += PLYProperty(nextLine(1), nextLine(2))
+            }
+            cnt += 1
+          }
+          fileElements += PLYElement(elementFormat.get, elementCount, elementProperties.toArray)
+        }
+      }
+      cnt += 1
+    }
+
+    val vertexInfo = fileElements.find(_.format == PLYElementFormat.VERTEX).get
+    val faceInfo = fileElements.find(_.format == PLYElementFormat.FACE).get
+    val faceDefined = validateElementFace(faceInfo)
+    val vertexDefined = validateElementVertex(vertexInfo)
+    if (!vertexDefined.status) {
+      throw new IOException(
+        "Unsupported element property provided"
+      )
+    } else if (!vertexDefined.is3DVertex) {
+      throw new IOException(
+        "Vertex (x,y,z) not defined in file."
+      )
+    } else if (!faceDefined) {
+      throw new IOException(
+        "Face element defined but no property defined."
+      )
+    }
+    HeaderInfo(
+      format = fileFormat.get,
+      vertexInfo = vertexInfo,
+      faceInfo = faceInfo,
+      comments = fileComments.toArray,
+      headerLength = header.map(_.getBytes("UTF-8").length).sum + header.length
+    )
+  }
+
+  private def validateElementFace(element: PLYElement): Boolean = {
+    if (element.count == 0) true
+    else {
+      element.properties.nonEmpty && element.properties.exists(prop => VERTEX_INDEX_PROPERTY_NAMES.contains(prop.name))
+    }
+  }
+
+  private def validateElementVertex(element: PLYElement): PLYItemsDefined = {
+    val xyz = Seq("x", "y", "z")
+    val n = Seq("nx", "ny", "nz")
+    val color = Seq("red", "green", "blue")
+    val colora = Seq("red", "green", "blue", "alpha")
+    val st = Seq("s", "t")
+    val uv = Seq("u", "v")
+    val texture = Seq("texture_u", "texture_v")
+    val all = xyz ++ n ++ color ++ colora ++ st ++ uv ++ texture
+    val names = element.properties.map(_.name)
+    val is3DVertexDefined = xyz.forall(names.contains)
+    val is3DNormalDefined = n.forall(names.contains)
+    val is3DVertexColorDefined = color.forall(names.contains) || colora.forall(names.contains)
+    val is3DUVsDefined =
+      st.forall(names.contains) ||
+        uv.forall(names.contains) ||
+        texture.forall(names.contains)
+    val status = element.properties.map(_.name).forall(item => all.contains(item))
+    PLYItemsDefined(status, is3DVertexDefined, is3DNormalDefined, is3DVertexColorDefined, is3DUVsDefined)
+  }
+
+}
---
+++ src/main/scala/scalismo/io/ply/PLYMeshReaderBinary.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2015 University of Basel, Graphics and Vision Research Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scalismo.io.ply
+
+import scalismo.color.RGBA
+import scalismo.common.PointId
+import scalismo.geometry.Point3D
+import scalismo.mesh.{TriangleCell, TriangleList, TriangleMesh3D}
+
+import java.io.IOException
+import java.nio.{ByteBuffer, ByteOrder}
+import scala.util.Try
+
+object PLYMeshReaderBinary {
+  def readPoints(buffer: ByteBuffer,
+                 vertexInfo: PLYElement,
+                 byteOrder: ByteOrder
+  ): (IndexedSeq[Point3D], Option[IndexedSeq[RGBA]]) = {
+    val pIndex = PLYHelpers.getVertexPropertyIndex(vertexInfo.properties)
+    val data: IndexedSeq[(Point3D, Option[RGBA])] = (0 until vertexInfo.count).map { _ =>
+      val data = vertexInfo.properties.map { item =>
+        item.read(buffer, byteOrder)
+      }
+      val p = Point3D(
+        data(pIndex.x).head.asInstanceOf[Float],
+        data(pIndex.y).head.asInstanceOf[Float],
+        data(pIndex.z).head.asInstanceOf[Float]
+      )
+      val c = if (pIndex.isRGB) {
+        Option(
+          RGBA(
+            data(pIndex.red.get).head.asInstanceOf[Int].toDouble / 255.0,
+            data(pIndex.green.get).head.asInstanceOf[Int].toDouble / 255.0,
+            data(pIndex.blue.get).head.asInstanceOf[Int].toDouble / 255.0,
+            1.0
+          )
+        )
+      } else if (pIndex.isRGBA) {
+        Option(
+          RGBA(
+            data(pIndex.red.get).head.asInstanceOf[Int].toDouble / 255.0,
+            data(pIndex.green.get).head.asInstanceOf[Int].toDouble / 255.0,
+            data(pIndex.blue.get).head.asInstanceOf[Int].toDouble / 255.0,
+            data(pIndex.alpha.get).head.asInstanceOf[Int].toDouble / 255.0
+          )
+        )
+      } else {
+        None
+      }
+      (p, c)
+    }
+    val (points, colors) = data.unzip
+    val flattenedColors: Option[IndexedSeq[RGBA]] = if (colors.exists(_.isEmpty)) None else Some(colors.flatten)
+    (points, flattenedColors)
+  }
+
+  def readTriangles(buffer: ByteBuffer, faceInfo: PLYElement, byteOrder: ByteOrder): IndexedSeq[TriangleCell] = {
+    if (faceInfo.count == 0) {
+      IndexedSeq.empty
+    } else {
+      val vertexIndex = PLYHelpers
+        .getFacePropertyIndex(faceInfo.properties)
+        .vertexIndex
+        .getOrElse(
+          throw new IOException(
+            "Faces property vertex_index not found."
+          )
+        )
+      (0 until faceInfo.count).map { _ =>
+        val data = faceInfo.properties.map { property =>
+          property.read(buffer, byteOrder)
+        }
+        val vertexData = data(vertexIndex)
+        if (vertexData.length != 3) {
+          throw new IOException(
+            "Faces elements different than 3 specified."
+          )
+        }
+        TriangleCell(PointId(vertexData(0).asInstanceOf[Int]),
+                     PointId(vertexData(1).asInstanceOf[Int]),
+                     PointId(vertexData(2).asInstanceOf[Int])
+        )
+      }
+    }
+  }
+}
