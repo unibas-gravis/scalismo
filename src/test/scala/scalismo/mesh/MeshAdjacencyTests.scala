9d882cf7dcd651e06673ed8c827ec0d8cf4d1fbb
Andreas Morel-Forster <andreas.morel@shapemeans.com>
2025-07-30 13:24:58 +0200

    added test for retrieving adjacent triangles for point ids in triangle meshes

---
+++ src/test/scala/scalismo/mesh/MeshAdjacencyTests.scala
@@ -0,0 +1,77 @@
+package scalismo.mesh
+
+import scalismo.ScalismoTestSuite
+import scalismo.common.PointId
+import scalismo.geometry.Point3D
+
+class MeshAdjacencyTests extends ScalismoTestSuite {
+
+  describe("a mesh adjacency operation") {
+
+    object Fixture {
+      val points = IndexedSeq(
+        Point3D(0,0,0),
+        Point3D(1,0,0),
+        Point3D(0,1,0),
+        Point3D(0,0,1)
+      )
+      val cells = TriangleList(IndexedSeq(
+        TriangleCell(PointId(0),PointId(1),PointId(2)),
+        TriangleCell(PointId(0),PointId(2),PointId(3)),
+      ))
+      val mesh = TriangleMesh3D(
+        points,
+        cells
+      )
+      val meshWithFreePoint = TriangleMesh3D(
+        points,
+        TriangleList(cells.triangles.drop(1))
+      )
+      val meshWithFreePointAtEnd = TriangleMesh3D(
+        points,
+        TriangleList(cells.triangles.dropRight(1))
+      )
+    }
+
+    it("should return the correct triangles for all points") {
+      val mesh = Fixture.mesh
+
+      val tests = Seq(
+        (PointId(0), IndexedSeq(TriangleId(0), TriangleId(1))),
+        (PointId(1), IndexedSeq(TriangleId(0))),
+        (PointId(2), IndexedSeq(TriangleId(0), TriangleId(1))),
+        (PointId(3), IndexedSeq(TriangleId(1)))
+      )
+      for( (pid, adjList) <- tests ) {
+        val adj = mesh.triangulation.adjacentTrianglesForPoint(pid)
+        adj.sortBy(_.id) shouldBe adjList
+      }
+    }
+
+    it("should return the correct triangles for points also not contained in a triangle") {
+      {
+        val mesh = Fixture.meshWithFreePoint
+
+        val tests = Seq((PointId(0), IndexedSeq(TriangleId(0))), (PointId(1), IndexedSeq()), (PointId(2), IndexedSeq(TriangleId(0))), (PointId(3), IndexedSeq(TriangleId(0))))
+        for ((pid, adjList) <- tests) {
+          val adj = mesh.triangulation.adjacentTrianglesForPoint(pid)
+          adj.sortBy(_.id) shouldBe adjList
+        }
+      }
+      {
+        val mesh = Fixture.meshWithFreePointAtEnd
+        val tests = Seq(
+          (PointId(0),  IndexedSeq(TriangleId(0))),
+          (PointId(1),  IndexedSeq(TriangleId(0))),
+          (PointId(2),  IndexedSeq(TriangleId(0))),
+          (PointId(3),  IndexedSeq()),
+        )
+        for ((pid, adjList) <- tests) {
+          val adj = mesh.triangulation.adjacentTrianglesForPoint(pid)
+          adj.sortBy(_.id) shouldBe adjList
+        }
+      }
+    }
+  }
+
+}
